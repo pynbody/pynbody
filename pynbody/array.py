4269c7798512378ad0b1ed6469f3e2b2744822e5
Andrew Pontzen <app26@ast.cam.ac.uk>
2010-11-30 19:15:36 +0000

    Python 2.5 backward compatibility

--- pynbody/array.py
+++ pynbody/array.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from .backcompat import property
+
 class SimArray(np.ndarray) :
     def __new__(subtype, data, units=None, **kwargs) :
 	new = np.array(data, **kwargs).view(subtype)
