- isolated models don't have 'a' appended to their units - fixed a small bug in filt.Sphere - added a rotateup function to snapshot

pynbody/snapshot.py
@@ -141,7 +141,15 @@ class SimSnap(object) :
 	self['vel'] = np.dot(matrix, vel.transpose()).transpose().view(array.SimArray)
 	
 	# could search for other 3D arrays here too?
-	
+
+
+    def rotateup(self, angle):
+        """Rotates the snapshot along the current x-axis by 'angle' degrees."""
+        angle *= np.pi/180
+        self.transform(np.matrix([[1,      0,             0],
+                                  [0, np.cos(angle), -np.sin(angle)],
+                                  [0, np.sin(angle),  np.cos(angle)]]))
+
 	
     def __len__(self) :
 	return self._num_particles