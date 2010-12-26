45143bba9d31b4e4a48c29fa409d3f49b640b17b
Andrew Pontzen <app26@ast.cam.ac.uk>
2010-12-26 12:39:32 +0000

    Add angular momentum derived array

--- pynbody/derived.py
+++ pynbody/derived.py
@@ -1,5 +1,7 @@
 from . import snapshot
 from snapshot import SimSnap
+from . import array
+import numpy as np
 
 @SimSnap.derived_quantity
 def r(self):
@@ -11,17 +13,26 @@ def rxy(self):
            
 @SimSnap.derived_quantity
 def vr(self):
+    """Radial velocity"""
     return (self['pos']*self['vel']).sum(axis=1)/self['r']
 
 @SimSnap.derived_quantity
 def v2(self) :
+    """Squared velocity"""
     return (self['vel']**2).sum(axis=1)
 
 @SimSnap.derived_quantity
 def ke(self) :
+    """Specific kinetic energy"""
     return 0.5*(self['vel']**2).sum(axis=1)
 
-
+@SimSnap.derived_quantity
+def j(self) :
+    """Specific angular momentum"""
+    angmom = np.cross(self['pos'], self['vel']).view(array.SimArray)
+    angmom.units = self['pos'].units*self['vel'].units
+    return angmom
+        
 @SimSnap.derived_quantity
 def vrxy(self):
     return (self['pos'][:,0:2]*self['vel'][:,0:2]).sum(axis=1)/self['r']
