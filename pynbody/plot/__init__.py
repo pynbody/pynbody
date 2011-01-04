96ded3b566006381773933db23bb27ccc693e931
Andrew Pontzen <app26@ast.cam.ac.uk>
2011-01-04 10:39:16 +0000

    Add circular velocity plotting, thus fixing issue 10

--- pynbody/plot/__init__.py
+++ pynbody/plot/__init__.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from . import sfh, phasediagram
+from . import sfh, phasediagram, profile
+
+reload(profile)
+
 from .sfh import *
+from .profile import rotation_curve
 
