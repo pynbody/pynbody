f34cf84b3899bfc068a6e93f50549036fb823b34
Andrew Pontzen <app26@ast.cam.ac.uk>
2011-01-11 15:58:56 +0000

    Update load factory to pass in keyword arguments

--- pynbody/__init__.py
+++ pynbody/__init__.py
@@ -16,11 +16,11 @@ from analysis import profile
 
 _snap_classes = [gadget.GadgetSnap, tipsy.TipsySnap]
 
-def load(filename, *args) :
+def load(filename, *args, **kwargs) :
     """Loads a file using the appropriate class, returning a SimSnap
     instance."""
     for c in _snap_classes :
-	if c._can_load(filename) : return c(filename,*args)
+	if c._can_load(filename) : return c(filename,*args,**kwargs)
 	
     raise RuntimeError("File format not understood")
 
