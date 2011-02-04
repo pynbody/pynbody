9ee6a0193811235ee6c98882b4b94298fb9a315a
Andrew Pontzen <app26@ast.cam.ac.uk>
2011-02-04 11:50:47 +0000

    New attempt at fixing issue 25

--- pynbody/array.py
+++ pynbody/array.py
@@ -551,6 +551,9 @@ def _wrap_fn(w) :
 	 # but it seems required on McMaster setup (Py 2.6.5, NP 1.4.1)
 	 if kw!={} :
 	     return w(SimArray(s), *y, **kw)
+	 else :
+	     return w(SimArray(s), *y)
+	 
      q.__name__ = w.__name__
      return q
 
