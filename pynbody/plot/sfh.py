7045756c57034338f2d1afb513c3811a756f293a
Greg Stinson <stinsongr@gmail.com>
2011-02-22 13:48:39 +0000

    fixed units/range issues so that SFH is calculated correctly
    still needs support/option to use starlog file

--- pynbody/plot/sfh.py
+++ pynbody/plot/sfh.py
@@ -2,11 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def sfh(t,filename=None,**kwargs):
+    nbins=100
     sfhist, bins, patches = plt.hist(t.star['tform'].in_units("Gyr"),
-                                     weights=t.star['mass'].in_units('Msol')*1e-9,
-                                     bins=500,histtype='step',color='k')
+                                     weights=t.star['mass'].in_units('Msol')*1e-9*nbins / (t.star['tform'].in_units("Gyr").max() - t.star['tform'].in_units("Gyr").min()),
+                                     bins=nbins,histtype='step',color='k')
     plt.xlabel('Time [Gyr]')
     plt.ylabel('SFR [M$_\odot$ yr$^{-1}$]')
     if (filename): plt.savefig(filename,**kwargs)
-    else: plt.show()
-    return plt
