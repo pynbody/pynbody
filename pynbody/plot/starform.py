5ceab781f05180e2baa3510e76d08d703988ffa5
Greg Stinson <stinsongr@gmail.com>
2011-02-23 18:44:59 +0000

    sfh and schmidtlaw done.  should be tested on cosmological simulations

--- pynbody/plot/starform.py
+++ pynbody/plot/starform.py
@@ -3,40 +3,60 @@ import matplotlib.pyplot as plt
 from ..analysis import angmom
 from ..analysis import profile
 
-def sfh(t,filename=None,**kwargs):
+def sfh(t,filename=None,massform=True,**kwargs):
+    '''star formation history
+    Usage:
+    import pynbody.plot as pp
+    pp.sfh(s,linestyle='dashed',color='k')
+
+    By default, sfh will use the formation mass of the star.  In tipsy, this will be
+    taken from the starlog file.  Set massform=False if you want the final (observed)
+    star formation history
+    '''
     nbins=100
-    sfhist, bins, patches = plt.hist(t.star['tform'].in_units("Gyr"),
-                                     weights=t.star['mass'].in_units('Msol')*1e-9*nbins / (t.star['tform'].in_units("Gyr").max() - t.star['tform'].in_units("Gyr").min()),
-                                     bins=nbins,histtype='step',color='k')
+    binnorm = 1e-9*nbins / (t.star['tform'].in_units("Gyr").max() - t.star['tform'].in_units("Gyr").min())
+    if massform :
+        try:
+            weight = t.star['massform'].in_units('Msol') * binnorm
+        except KeyError :
+            weight = t.star['mass'].in_units('Msol') * binnorm
+    else:
+        weight = t.star['mass'].in_units('Msol') * binnorm
+                                                               
+    sfhist, bins, patches = plt.hist(t.star['tform'].in_units("Gyr"),weights=weight,
+                                     bins=nbins,histtype='step',**kwargs)
     plt.xlabel('Time [Gyr]')
     plt.ylabel('SFR [M$_\odot$ yr$^{-1}$]')
-    if (filename): plt.savefig(filename,**kwargs)
+    if (filename): plt.savefig(filename)
 
 
-def schmidtlaw(t,center=True,filename=None,pretime=50,diskheight=3,maxr=20,radial=True,**kwargs):
+def schmidtlaw(t,center=True,filename=None,pretime=50,diskheight=3,rmax=20,radial=True,**kwargs):
     if center :
         angmom.faceon(t)
     
     # select stuff
-    youngstars = np.where(t.star['tform'].in_units("Myr") > t.properties['time'].in_units("Myr") - pretime)
-    diskstars = np.where((np.abs(t.star['z'].in_units("kpc")) < diskheight) and (t.star['rxy'] < rmax))
+    youngstars = np.where(t.star['tform'].in_units("Myr") > t.properties['time'].in_units("Myr") - pretime)[0]
+    # * means "and"
+    diskstars = np.where((np.abs(t.star['z'].in_units("kpc")) < diskheight) * (t.star['rxy'] < rmax))[0]
     youngdiskstars = np.intersect1d(youngstars, diskstars)
     
-    diskgas = np.where((np.abs(t.gas['z'].in_units("kpc")) < diskheight) and (t.gas['rxy'] < rmax))
+    diskgas = np.where((np.abs(t.gas['z'].in_units("kpc")) < diskheight) * (t.gas['rxy'] < rmax))[0]
 
     # calculate surface densities
     if radial :
         ps = profile.Profile(t.star[youngdiskstars])
-        ps.rho
         pg = profile.Profile(t.gas[diskgas])
-        pg.rho
     else :
         gashist, x, y = np.histogram2d(sim.gas['x'][diskgas], sim.gas['y'][diskgas],bins=nbins,range=[t_range,rho_range])
         starhist, x, y = np.histogram2d(sim.star['x'][youngdiskstars], sim.star['y'][youngdiskstars],bins=nbins,range=[t_range,rho_range])
 
-    plt.plot(pg.rho,ps.rho,mark="+")
+    plt.loglog(pg['den'].in_units('Msol pc^-2'),ps['den'].in_units('Msol kpc^-2') / pretime/1e6,"+")
+    xsigma = 10.0**np.linspace(np.log10(pg['den'].min()),np.log10(pg['den'].max()),100)
     ysigma=2.5e-4*xsigma**1.5        # Kennicutt (1998)
     ysigmanew=10.**(-2.1)*xsigma**1.0   # Bigiel et al (2007)
-    plt.plot(xsigma,ysigma)
-    plt.plot(xsigma,ysigmanew,line="--")
+    plt.loglog(xsigma,ysigma,label='Kennicutt (1998)')
+    plt.loglog(xsigma,ysigmanew,linestyle="dashed",label='Bigiel et al (2007)')
+    plt.xlabel('$\Sigma_{gas}$ [M$_\odot$ yr$^{-1}$ kpc$^{-2}$]')
+    plt.ylabel('$\Sigma_{SFR}$ [M$_\odot$ pc$^{-2}$]')
+    plt.legend(loc=2)
 
