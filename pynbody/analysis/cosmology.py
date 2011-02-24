2d2590edb8412c5ec7364911629be845992476b2
universeofreed <universeofreed@gmail.com>
2011-02-24 14:48:53 +0100

    added linear growth factor to cosmology.py (for flat lambda cosmology)

--- pynbody/analysis/cosmology.py
+++ pynbody/analysis/cosmology.py
@@ -1,5 +1,5 @@
 import math, numpy as np
-
+numpy=np ## alias the alias 
 from .. import units
 
 def _a_dot(a, h0, om_m, om_l) :
@@ -8,6 +8,54 @@ def _a_dot(a, h0, om_m, om_l) :
 
 def _a_dot_recip(*args) :
     return 1./_a_dot(*args)
+
+def hzoverh0(a, omegam0): 
+    """ returns: H(a) / H0  = [omegam/a**3 + (1-omegam)]**0.5 """
+    return numpy.sqrt(omegam0*numpy.power(a,-3) + (1.-omegam0))
+
+def lingrowthintegral(a,omegam0):   
+    """ (e.g. eq. 8 in lukic et al. 2008)   returns: da / [a*H(a)/H0]**3 """
+    return numpy.power((a * hzoverh0(a,omegam0)),-3)
+
+def lingrowthfac(red,omegam0,omegal0):
+    """
+    returns: linear growth factor, b(a) normalized to 1 at z=0, good for flat lambda only
+    a = 1/1+z
+    b(a) = Delta(a) / Delta(a=1)   [ so that b(z=0) = 1 ]
+    (and b(a) [Einstein de Sitter, omegam=1] = a)
+    
+    Delta(a) = 5 omegam / 2 H(a) / H(0) * integral[0:a] [da / [a H(a) H0]**3]
+    equation  from  peebles 1980 (or e.g. eq. 8 in lukic et al. 2008) """
+##  need to add w ~= , nonflat, -1 functionality
+    
+    import scipy.integrate
+
+    if ((omegam0 + omegal0) != 1.):
+        print "WARNING -- omegam + lambda not equal 1, solution NOT VALID"
+    
+    a = 1/(1.+red)
+    
+    ## 1st calc. for z=z
+    lingrowth = scipy.integrate.quad(lingrowthintegral,0.,a, (omegam0))[0]
+    lingrowth *= 5./2. * omegam0 * hzoverh0(a,omegam0)
+
+    ## then calc. for z=0 (for normalization)
+    a0 = 1.
+    lingrowtha0 =  scipy.integrate.quad(lingrowthintegral,0.,a0, (omegam0))[0]
+    lingrowtha0 *= 5./2. * omegam0  * hzoverh0(a0,omegam0)
+
+    lingrowthfactor = lingrowth / lingrowtha0
+    return lingrowthfactor
+
+
+def getlingrowthfactor(f,z=None):  ##  this is just a wrapper for pynbody.
+    ## returns: linear growth factor, b(a) normalized to 1 at z=0, good for flat lambda only
+    if z is None :
+        red = f.properties['z']
+    omegam0 = f.properties['omegaM0']
+    omegal0 = f.properties['omegaL0']
+    return lingrowthfac(red,omegam0,omegal0)
+
 
 def age(f, z=None, unit='Gyr') :
     """Calculate the age of the universe in the snapshot f
