Bug fixes to decomp routine's loading on-disk rotation curves

pynbody/analysis/decomp.py
@@ -86,10 +86,10 @@ def decomp(h, aligned=False, j_disk_min = 0.8, j_disk_max=1.1, E_cut = None, j_c
         r, v_c = np.loadtxt(h.ancestor.filename+".rot."+str(h.properties["halo_id"]), skiprows=1, unpack=True)
         
         pro_d = profile.Profile(d, nbins=100, type='log')
-        r_x = pro_d.rbins.in_units("kpc")
+        r_me = pro_d.rbins.in_units("kpc")
         r_x = np.concatenate(([0], r, [r.max()*2]))
         v_c = np.concatenate(([v_c[0]], v_c, [v_c[-1]]))
-        v_c = interp.interp1d(r_x, v_c, bounds_error=False)(pro_d.rbins)
+        v_c = interp.interp1d(r_x, v_c, bounds_error=False)(r_me)
         
         if verbose :
             print "   -- found existing rotation curve on disk, using that"
@@ -97,9 +97,10 @@ def decomp(h, aligned=False, j_disk_min = 0.8, j_disk_max=1.1, E_cut = None, j_c
         v_c = v_c.view(array.SimArray)
         v_c.units = "km s^-1"
         v_c.sim = d
-    
+
+        v_c.convert_units(h['vel'].units)
+        
         pro_d._profiles['v_circ'] = v_c
-        print v_c
         pro_d.v_circ_loaded = True
         
     except IOError :