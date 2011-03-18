365d3b90e4271095a998fd13060c0ce868b23cd2
Rok Roskar <roskar@physik.uzh.ch>
2011-03-18 15:11:37 +0100

    added radius control to rotation_curve plotting

--- pynbody/plot/profile.py
+++ pynbody/plot/profile.py
@@ -7,7 +7,7 @@ import pylab as p
 
 def rotation_curve(s, center=True, r_units = 'kpc',
                    v_units = 'km s^-1', disk_height='100 pc', nbins=50,
-                   bin_spacing = 'equaln') :
+                   bin_spacing = 'equaln', **kwargs) :
     """Centre on potential minimum, align so that the disk is in the
     x-y plane, then use the potential in that plane to generate and
     plot a rotation curve."""
@@ -15,9 +15,16 @@ def rotation_curve(s, center=True, r_units = 'kpc',
     if center :
         angmom.faceon(s)
 
+    if 'min' in kwargs :
+        min_r = kwargs['min']
+    else:
+        min_r = s['rxy'].min()
+    if 'max' in kwargs :
+        max_r = kwargs['max']
+    else:
+        max_r = s['rxy'].max()
 
-
-    pro = profile.Profile(s, nbins = nbins, type=bin_spacing)
+    pro = profile.Profile(s, min = min_r, max = max_r, nbins = nbins, type=bin_spacing)
 
     r = pro['rbins'].in_units(r_units)
     v = pro['v_circ'].in_units(v_units)
