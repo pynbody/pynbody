af298bb294bdc50b21b85f0669182a465f8584f2
Rok Roskar <roskar@physik.uzh.ch>
2011-03-18 14:44:38 +0100

    added sph image rendering routines to __init__

--- pynbody/plot/__init__.py
+++ pynbody/plot/__init__.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from . import stars, phasediagram, profile
+from . import stars, phasediagram, profile, sph
 
 reload(profile)
 reload(phasediagram)
 reload(stars)
+reload(sph)
 
 from .stars import sfh, schmidtlaw
 from .profile import rotation_curve
 from .phasediagram import rho_T
 from .generic import hist2d
+from .sph import image
+from .sph import faceon_image
+from .sph import sideon_image
