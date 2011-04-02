0189700893c9658ce347e213642fe0f8d8b489ec
Andrew Pontzen <app26@ast.cam.ac.uk>
2011-04-02 18:54:03 +0100

    Remove references to TipsySnap in snapshot_test.py -- more generic tests by creating, rather than loading, snapshot

--- nose/snapshot_test.py
+++ nose/snapshot_test.py
@@ -3,54 +3,14 @@ import numpy as np
 
 def setup() :
     global f, h
-    f = pynbody.load("MW1.512/MW1.512g1bwdcK.00512")
-    h = f.halos()
-    assert type(h) is pynbody.halo.AmigaGrpCatalogue
+    f = pynbody.new(dm=1000, star=500, gas=500)
+    f['pos'] = np.random.normal(scale=1.0, size=f['pos'].shape)
+    f['vel'] = np.random.normal(scale=1.0, size=f['vel'].shape)
+    f['mass'] = np.random.uniform(1.0,10.0,size=f['mass'].shape)
 
 def teardown() :
     global f
     del f
-
-
-def test_get() :
-    current =  f['pos'][0:100:10]
-    correct = np.array([[ 0.05419805, -0.0646539,  -0.15700017],
-                        [ 0.04330373, -0.05328381, -0.13771422],
-                        [ 0.05118063, -0.05374216, -0.13027677],
-                        [ 0.0578489,  -0.05937488, -0.1415794 ],
-                        [ 0.0898305,  -0.08368044, -0.19480452],
-                        [ 0.06412952, -0.0712738,  -0.15869184],
-                        [ 0.08050784, -0.0779544,  -0.18164405],
-                        [ 0.06102384, -0.06157777, -0.14551604],
-                        [ 0.06705956, -0.07371401, -0.1634458 ],
-                        [ 0.05734131, -0.06026748, -0.1280915 ]])
-    
-    print "Position error of ",np.abs(current-correct).sum()
-    assert (np.abs(current-correct).sum()<1.e-7)
-
-def test_standard_arrays() :
-    # just check all standard arrays are there
-    with f.lazy_off :
-        f['x']
-        f['y']
-        f['z']
-        f['pos']
-        f['vel']
-        f['vx']
-        f['vy']
-        f['vz']
-        f['eps']
-        f['phi']
-        f['mass']
-        f.gas['rho']
-        f.gas['temp']
-        f.gas['metals']
-        f.star['tform']
-        f.star['metals']
-    
-    
-def test_halo() :
-    assert len(h[1])==444703
 
     
 def test_subscript() :
@@ -82,19 +42,28 @@ def test_derived_array() :
     assert 'vr' in f.derivable_keys()
     f['vr']
     assert 'vr' in f.keys()
+    assert f['vr'].derived
+    assert f.gas['vr'].derived
+    assert f[::4]['vr'].derived
+    assert f[[2,3,7,12]]['vr'].derived
     f['pos']+=(2,0,0)
     assert 'vr' not in f.keys()
-
+    f['pos']-=(2,0,0)
+    f.gas['vr']
+    assert f.gas['vr'].derived
+    assert 'vr' not in f.keys()
+    assert 'vr' in f.gas.keys()
+    f['vr']
+    assert 'vr' in f.keys()
+    
     try:
         f['r'][22]+=(3,3,3)
         # Array should not be writable
         assert False
     except RuntimeError :
         pass
-    
 
-def test_loadable_array() :
-    assert 'HI' in f.loadable_keys()
-    f['HI']
-    assert 'HI' in f.keys()
-    
+
+def test_unit_inheritance() :
+    f['pos'].units = 'km'
+    f['vel'].units = 'km s^-1'
