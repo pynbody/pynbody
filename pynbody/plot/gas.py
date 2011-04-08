cleaned up a bunch of the plots.  Fixed units in quick v_c profile

pynbody/plot/gas.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def rho_T(sim, nbins=100, nlevels = 20, log=True, clear=True, **kwargs):
+def rho_T(sim, nbins=100, nlevels = 20, log=True, clear=True, 
+          filename=None, rho_units="m_p cm**-3",**kwargs):
     """
     Plot Temperature vs. Density for the gas particles in the snapshot.
 
@@ -24,19 +25,26 @@ def rho_T(sim, nbins=100, nlevels = 20, log=True, clear=True, **kwargs):
     import matplotlib.pyplot as plt
     from matplotlib import ticker, colors
 
-
+    print rho_units
+    sim.gas['rho'].sim.gas['rho'].convert_units(rho_units)
+    print sim.gas['rho'].units
     if kwargs.has_key('t_range'):
         t_range = kwargs['t_range']
         assert len(t_range) == 2
     else:
-        t_range = (np.log10(np.min(sim.gas['temp'])),np.log10(np.max(sim.gas['temp'])))
+        t_range = (np.log10(np.min(sim.gas['temp'])),
+                   np.log10(np.max(sim.gas['temp'])))
     if kwargs.has_key('rho_range'):
         rho_range = kwargs['rho_range']
         assert len(rho_range) == 2
     else:
-        rho_range = (np.log10(np.min(sim.gas['rho'])), np.log10(np.max(sim.gas['rho'])))
+        rho_range = (np.log10(np.min(sim.gas['rho'])), 
+                     np.log10(np.max(sim.gas['rho'])))
 
-    hist, x, y = np.histogram2d(np.log10(sim.gas['temp']), np.log10(sim.gas['rho']),bins=nbins,range=[t_range,rho_range])
+    print sim.gas['rho'].units
+    hist, x, y = np.histogram2d(np.log10(sim.gas['temp']), 
+                                np.log10(sim.gas['rho']),
+                                bins=nbins, range=[t_range,rho_range])
 
 
     if log:
@@ -59,8 +67,12 @@ def rho_T(sim, nbins=100, nlevels = 20, log=True, clear=True, **kwargs):
                                                           # are swapped
                      hist, levels, norm=cont_color)
 
+    print sim.gas['rho'].units
 
-    plt.xlabel(r'$log_{10}(\rho/'+sim.gas['rho'].units.latex()+')$')
-    plt.ylabel(r'$log_{10}(T/'+sim.gas['temp'].units.latex()+')$')
+    plt.xlabel(r'log$_{10}$($\rho$/$'+sim.gas['rho'].units.latex()+'$)')
+    plt.ylabel(r'log$_{10}$(T/$'+sim.gas['temp'].units.latex()+'$)')
     plt.xlim((rho_range[0],rho_range[1]))
     plt.ylim((t_range[0],t_range[1]))
+    if (filename): 
+        print "Saving "+filename
+        plt.savefig(filename)

pynbody/plot/stars.py
@@ -67,8 +67,8 @@ def schmidtlaw(sim,center=True,filename=None,pretime='50 Myr',
 
     # calculate surface densities
     if radial :
-        ps = profile.Profile(diskstars[youngstars])
-        pg = profile.Profile(diskgas)
+        ps = profile.Profile(diskstars[youngstars],nbins=10)
+        pg = profile.Profile(diskgas,nbins=10)
     else :
         # make bins 2 kpc
         nbins = rmax * 2 / binsize
@@ -146,17 +146,19 @@ def satlf(sim,band='R',filename=None, compare=True,clear=True,**kwargs) :
             raise IOError, "cmdlum.npz not found"
         plt.semilogy(sorted(tolmags),np.arange(len(tolmags)),
                      label='MW (Tollerud et al 1998)')
-        
-        xmag = np.linspace(min(halomags),max(halomags),100)
+
+        halomags = np.array(halomags)
+        halomags = halomags[np.asarray(np.where(np.isfinite(halomags)))]
+        xmag = np.linspace(halomags.min(),halomags.max(),100)
         # Trentham + Tully (2009) equation 6
         # number of dwarfs between -11>M_R>-17 is well correlated with mass
         logNd = 0.91*np.log10(sim.properties['mass'])-10.2
         # set Nd from each equal to combine Trentham + Tully with Koposov
         coeff = 10.0**logNd / (10**-0.6 - 10**-1.2)
+
         print 'Koposov coefficient:'+str(coeff)
         # Analytic expression for MW from Koposov
         yn=coeff * 10**((xmag+5.0)/10.0) # Koposov et al (2007)
-        #100.0/np.log(10.0)
         plt.semilogy(xmag,yn,linestyle="dashed",
                      label='Koposov et al (2007) + Trentham & Tully (2009)')
 