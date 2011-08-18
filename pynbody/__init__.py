c0be764d2daa9c52b5678d739662be52ab87978b
Andrew Pontzen <app26@ast.cam.ac.uk>
2011-08-18 22:21:49 +0100

    Basic python 3 compatibility fixes and tidy-up for main __init__.py

--- pynbody/__init__.py
+++ pynbody/__init__.py
@@ -1,6 +1,15 @@
-import ConfigParser, os
+# Import basic dependencies
+import ConfigParser
+import os
+import imp
+import numpy
 
+# Create config dictionaries which will be required by subpackages
 config_parser = ConfigParser.ConfigParser()
+config = {}
+
+
+# Process configuration options
 config_parser.optionxform = str
 config_parser.read(os.path.join(os.path.dirname(__file__),"default_config.ini"))
 config_parser.read(os.path.join(os.path.dirname(__file__),"config.ini"))
@@ -8,8 +17,8 @@ config_parser.read(os.path.expanduser("~/.pynbodyrc"))
 config_parser.read("config.ini")
 
 
-config= {'verbose': config_parser.getboolean('general','verbose'),
-         'centering-scheme': config_parser.get('general','centering-scheme')}
+config.update({'verbose': config_parser.getboolean('general','verbose'),
+         'centering-scheme': config_parser.get('general','centering-scheme')})
 
 config['snap-class-priority'] = map(str.strip,
                                     config_parser.get('general', 'snap-class-priority').split(","))
@@ -22,26 +31,31 @@ for k in config_parser.options('default-cosmology') :
     config['default-cosmology'][k] = float(config_parser.get('default-cosmology', k))
 
 
-import util, filt, array, family, snapshot,  tipsy, gadget, gadgethdf, analysis, halo, derived, bridge, plot
+# Import subpackages
+from . import util, filt, array, family, snapshot,  tipsy, gadget, gadgethdf, analysis, halo, derived, bridge, plot
 
 # The following code resolves inter-dependencies when reloading
-reload(array)
-reload(util)
-# reload(family) # reloading this causes problems for active snapshots
-reload(snapshot)
-reload(tipsy)
-reload(gadget)
-reload(gadgethdf)
-reload(filt)
-reload(analysis)
-reload(halo)
-reload(derived)
-reload(bridge)
-reload(plot)
+imp.reload(array)
+imp.reload(util)
+# imp.reload(family) # reloading this causes problems for active snapshots
+imp.reload(snapshot)
+imp.reload(tipsy)
+imp.reload(gadget)
+imp.reload(gadgethdf)
+imp.reload(filt)
+imp.reload(analysis)
+imp.reload(halo)
+imp.reload(derived)
+imp.reload(bridge)
+imp.reload(plot)
 
-from analysis import profile
+# from analysis import profile
 
+
+# This is our definitive list of classes which are able to
+# load snapshots
 _snap_classes = [gadgethdf.GadgetHDFSnap, gadget.GadgetSnap, tipsy.TipsySnap]
+
 
 # Turn the config strings for snapshot/halo classes into lists of
 # actual classes
@@ -49,7 +63,6 @@ _snap_classes_dict = dict([(x.__name__,x) for x in _snap_classes])
 _halo_classes_dict = dict([(x.__name__,x) for x in halo._halo_classes])
 config['snap-class-priority'] = [_snap_classes_dict[x] for x in config['snap-class-priority']]
 config['halo-class-priority'] = [_halo_classes_dict[x] for x in config['halo-class-priority']]
-
 
 
 def load(filename, *args, **kwargs) :
