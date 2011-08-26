185c26fec46741c46270369aeb6ca1c4d2417ea0
Greg Stinson <stinsongr@gmail.com>
2011-08-26 17:14:08 +0200

    added cosmo.py, has a couple more things that cosmology, but doesn't support units

--- pynbody/analysis/__init__.py
+++ pynbody/analysis/__init__.py
@@ -1,11 +1,14 @@
 import imp
-from . import profile, fourier_decomp, cosmology, halo, luminosity, ionfrac
+from . import profile, fourier_decomp, cosmology, halo, luminosity
+from . import ionfrac, cosmo
 from .decomp import decomp
 
 imp.reload(profile)
 imp.reload(fourier_decomp)
 imp.reload(cosmology)
+imp.reload(cosmo)
 imp.reload(halo)
 imp.reload(luminosity)
 imp.reload(ionfrac)
 
+import numpy as np
