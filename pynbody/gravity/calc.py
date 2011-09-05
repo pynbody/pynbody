917f5f18afbe3122dfc2248832b9de891826cda2
Andrew Pontzen <app26@ast.cam.ac.uk>
2011-09-05 09:41:53 +0100

    Add PM gravity solver. Make gravity solver routines return units. Change sign in gravity solvers so that acceleration, rather than -acceleration, is returned.

--- pynbody/gravity/calc.py
+++ pynbody/gravity/calc.py
@@ -5,6 +5,60 @@ import math
 import tree
 import numpy as np
 
+def all_direct(f, eps=None) :
+    phi, acc = direct(f, f['pos'].view(np.ndarray), eps)
+    f['phi'] = phi
+    f['acc'] = acc
+
+def all_pm(f, eps=None, ngrid=10) :
+    phi, acc = pm(f, f['pos'].view(np.ndarray), eps, ngrid=ngrid)
+    f['phi'] = phi
+    f['acc'] = acc
+
+def pm(f, ipos, eps=None, ngrid = 10, x0=-1, x1=1) :
+    dx = float(x1-x0)/ngrid
+    grid, edges = np.histogramdd(f['pos'],
+                                 bins=ngrid,
+                                 range=[(x0,x1),(x0,x1),(x0,x1)],
+                                 normed=False,
+                                 weights = f['mass'])
+    grid/=dx**3
+    recip_rho_grid = np.fft.rfftn(grid)
+
+    freqs = np.fft.fftfreq(ngrid, d=dx)
+    
+    kvecs = np.zeros((ngrid,ngrid,ngrid/2+1,3))
+    kvecs[:,:,:,0] = freqs.reshape((1,ngrid,1,1))
+    kvecs[:,:,:,1] = freqs.reshape((1,1,ngrid,1))
+    kvecs[:,:,:,2] = abs(freqs[:ngrid/2+1].reshape((1,1,1,ngrid/2+1)))
+  
+    k = (kvecs**2).sum(axis=3)
+    assert k.shape==recip_rho_grid.shape
+    
+    recip_phi_grid = 4*math.pi*recip_rho_grid/k**2
+    recip_phi_grid[np.where(k==0)] = 0
+
+    phi_grid = np.fft.irfftn(recip_phi_grid, grid.shape)
+    grad_phi_grid = np.concatenate((np.fft.irfftn(-1.j*kvecs[:,:,:,0]*recip_phi_grid,grid.shape)[:,:,:,np.newaxis],
+                                    np.fft.irfftn(-1.j*kvecs[:,:,:,1]*recip_phi_grid,grid.shape)[:,:,:,np.newaxis],
+                                    np.fft.irfftn(-1.j*kvecs[:,:,:,2]*recip_phi_grid,grid.shape)[:,:,:,np.newaxis]),
+                                   axis=3)
+
+    
+    ipos_I = np.array((ipos-x0)/dx,dtype=int)
+    
+    phi = np.array([phi_grid[x,y,z] for x,y,z in ipos_I])
+    grad_phi = np.array([grad_phi_grid[x,y,z,:] for x,y,z in ipos_I])
+    
+    phi = phi.view(array.SimArray)
+    phi.units = units.G*f['mass'].units/f['pos'].units
+
+    grad_phi = grad_phi.view(array.SimArray)
+    grad_phi.units = units.G*f['mass'].units/f['pos'].units**2
+
+    return phi, -grad_phi
+    
+       
 def direct(f, ipos, eps= None) :
     try :
         import scipy, scipy.weave
@@ -22,7 +76,15 @@ def direct(f, ipos, eps= None) :
 
     code = file(os.path.join(os.path.dirname(__file__),'direct.c')).read()
     inline(code,['nips','n','pos','mass','ipos','epssq','m_by_r','m_by_r2'])
-    return m_by_r, m_by_r2
+    m_by_r = m_by_r.view(array.SimArray)
+    m_by_r2 = m_by_r2.view(array.SimArray)
+    m_by_r.units = f['mass'].units/f['pos'].units
+    m_by_r2.units = f['mass'].units/f['pos'].units**2
+
+    m_by_r*=units.G
+    m_by_r2*=units.G
+    
+    return m_by_r, -m_by_r2
 
 def treecalc(f, rs, eps= None) :
     gtree = tree.GravTree(f['pos'].view(np.ndarray),f['mass'].view(np.ndarray),eps=np.min(eps))
@@ -40,7 +102,7 @@ def midplane_rot_curve(f, rxy_points, eps = None, mode='tree') :
     if isinstance(eps, str) :
         eps = units.Unit(eps)
 
-    u_out = (units.G * f['mass'].units / f['pos'].units)**(1,2)
+    # u_out = (units.G * f['mass'].units / f['pos'].units)**(1,2)
     
     # Do four samples like Tipsy does
     rs = [pos for r in rxy_points for pos in [(r,0,0), (0,r,0), (-r,0,0), (0,-r,0)]]
@@ -52,13 +114,11 @@ def midplane_rot_curve(f, rxy_points, eps = None, mode='tree') :
     except KeyError :
         fn = mode
 
-    #if mode == 'tree':
-    #else:
-    #    m_by_r, m_by_r2 = fn(f,np.array(rs), eps=np.min(eps))
+    pot, accel = fn(f,np.array(rs), eps=np.min(eps))
 
-    m_by_r, m_by_r2 = fn(f,np.array(rs), eps=np.min(eps))
+    u_out = (accel.units*f['pos'].units)**(1,2)
     
-    accel = array.SimArray(m_by_r2,units.G * f['mass'].units / (f['pos'].units**2) )
+    # accel = array.SimArray(m_by_r2,units.G * f['mass'].units / (f['pos'].units**2) )
 
     vels = []
 
@@ -66,7 +126,7 @@ def midplane_rot_curve(f, rxy_points, eps = None, mode='tree') :
     for r in rxy_points:
         r_acc_r = []
         for pos in [(r,0,0), (0,r,0), (-r,0,0), (0,-r,0)]:
-            r_acc_r.append(np.dot(accel[i,:],pos))
+            r_acc_r.append(np.dot(-accel[i,:],pos))
             i = i+1
         
         vel2 = np.mean(r_acc_r)
@@ -75,7 +135,6 @@ def midplane_rot_curve(f, rxy_points, eps = None, mode='tree') :
         else :
             vel = 0
 
-        import pdb; pdb.set_trace()
         vels.append(vel)
 
     x = array.SimArray(vels, units = u_out)
