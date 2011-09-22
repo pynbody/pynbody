c654bb5584d510fafef7062355fbe875fe155a3d
Andrew Pontzen <app26@ast.cam.ac.uk>
2011-09-22 10:33:06 +0100

    Move cosmo.py to pkdgrav_cosmo.py to be explicit about why a second cosmology module exists

--- pynbody/analysis/__init__.py
+++ pynbody/analysis/__init__.py
@@ -1,12 +1,12 @@
 import imp
 from . import profile, fourier_decomp, cosmology, halo, luminosity
-from . import ionfrac, cosmo
+from . import ionfrac, pkdgrav_cosmo
 from .decomp import decomp
 
 imp.reload(profile)
 imp.reload(fourier_decomp)
 imp.reload(cosmology)
-imp.reload(cosmo)
+imp.reload(pkdgrav_cosmo)
 imp.reload(halo)
 imp.reload(luminosity)
 imp.reload(ionfrac)
