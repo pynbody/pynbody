63fd0150c4f64a6b93a7bcae7344da3da5e837c7
Greg Stinson <stinsongr@gmail.com>
2012-04-05 15:19:04 +0200

    attempt at adding pkdgrav

--- pynbody/gravity/tree.py
+++ pynbody/gravity/tree.py
@@ -1,7 +1,7 @@
 """Gravity Tree. Builds tree based on pkdgrav2"""
 
 try :
-    from .. import grav
+    from .. import pkdgrav
 except ImportError :
     import warnings
     warnings.warn("Unable to import PKDGrav gravity solver. Most likely this means either that your installation is broken, or that you are running python inside the pynbody distribution directory, in which case python cannot see the installed version. However, it also doesn't matter unless you want to use the tree gravity solver.",RuntimeWarning)
@@ -11,15 +11,16 @@ import numpy as np
 
 class GravTree:
     
-    def __init__(self, pos, mass, eps=None, leafsize=16):
+    def __init__(self, pos, mass, rs, eps=None, leafsize=16):
+        import pdb; pdb.set_trace()
         if config['tracktime']:
             import time
             start = time.clock()
-            self.tree = grav.treeinit(pos, mass, int(leafsize),int(len(mass)),np.float32(eps))
+            self.tree = pkdgrav.pkdPythonInitialize(pos, mass, eps, rs, int(leafsize))
             end = time.clock()
             if config['verbose']: print 'Tree build done in %5.3g s'%(end-start)
         else:
-            self.tree = grav.treeinit(pos, mass, int(leafsize),int(len(mass)),np.float32(eps))
+            self.tree = pkdgrav.pkdPythonInitialize(pos, mass, eps, rs, int(leafsize))
 
         self.derived = True
         self.flags = {'WRITEABLE':False}
@@ -31,14 +32,14 @@ class GravTree:
         if config['tracktime']:
             import time
             start = time.clock()
-            grav.calculate(self.tree, np.array(vec_pos), accel, pot, eps, len(vec_pos))
+            pkdgrav.pkdPythonDoGravity(self.tree, accel, pot, theta=0.55)
             end = time.clock()
             if config['verbose']: print 'Gravity calculated in %5.3g s'%(end-start)
         else:
-            grav.calculate(self.tree, np.array(vec_pos), accel, pot, eps)
+            pkdgrav.pkdPythonDoGravity(self.tree, accel, pot, theta=0.55)
         return accel, pot
         
     def __del__(self):
         if hasattr(self, 'tree'):
-            grav.free(self.tree)
+            pkdgrav.free(self.tree)
         
