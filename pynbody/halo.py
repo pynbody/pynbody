4b18b9f654eaa430931a2dc0c663b25b6dbbc192
Greg Stinson <stinsongr@gmail.com>
2012-04-05 15:00:55 +0200

    added support for the new AHF, eunit=0.03

--- pynbody/halo.py
+++ pynbody/halo.py
@@ -35,13 +35,21 @@ class HaloCatalogue(object) :
     def __init__(self) :
         self._halos = {}
 
-    def __getitem__(self, i) :
+    def calc_item(self, i):
         if self._halos.has_key(i) :  # and self._halos[i]() is not None :
             return self._halos[i] # ()
         else :
             h = self._get_halo(i)
             self._halos[i] = h # weakref.ref(h)
             return h
+
+    def __getitem__(self, item) :
+        if isinstance(item, slice):
+            indices = item.indices(len(self._halos))
+            [self.calc_item(i+1) for i in range(*indices)]
+            return self._halos[item]
+        else:
+            return self.calc_item(item)
 
     def is_subhalo(self,childid,parentid) :
         if (childid in self._halos[parentid].properties['children']) :
@@ -189,39 +197,83 @@ class AHFCatalogue(HaloCatalogue) :
 
     def _run_ahf(self, sim) :
         # if (sim is pynbody.tipsy.TipsySnap) :
+	typecode='90'
+        #elif (sim is pynbody.gadget.GadgetSnap):                               
+        #   typecode = '60' or '61'                                             
         import pynbody.units as units
+        # find AHFstep
+        for directory in os.environ["PATH"].split(os.pathsep) :
+            ahfs = glob.glob(os.path.join(directory,"AHF*"))
+            if (len(ahfs) == 1):
+                if (os.path.basename(ahfs[0]) == 'AHFstep'):
+                    isAHFstep=True
+                else: 
+                    isAHFstep=False
+                    break
+            if (len(ahfs) > 1):
+                for iahf, ahf in enumerate(ahfs):
+                    if (iahf == len(ahfs)-1): 
+                        groupfinder=ahf
+                        if (os.path.basename(ahfs[0]) == 'AHFstep'):
+                            isAHFstep=True
+                    if (os.path.basename(ahf) == 'AHFstep'): continue
+                    else: 
+                        isAHFstep=False
+                        groupfinder=ahf
+                        break
         #build units file
-        f = open('tipsy.info','w')
-        f.write(str(sim.properties['omegaM0'])+"\n")
-        f.write(str(sim.properties['omegaL0'])+"\n")
-        f.write(str(sim['pos'].units.ratio(units.kpc,a=1)/1000.0 * sim.properties['h'])+"\n")
-        f.write(str(sim['vel'].units.ratio(units.km/units.s,a=1))+"\n")
-        f.write(str(sim['mass'].units.ratio(units.Msol))+"\n")
-        f.close()
-        typecode='90'
-        #elif (sim is pynbody.gadget.GadgetSnap):
-        #   typecode = '60' or '61'
-        # make input file
-        f = open('AHFstep.in','w')
-        f.write(sim._filename+" "+typecode+" 1\n")
-        f.write(sim._filename+"\n256\n5\n5\n0\n0\n0\n0\n")
+        if isAHFstep:
+            f = open('tipsy.info','w')
+            f.write(str(sim.properties['omegaM0'])+"\n")
+            f.write(str(sim.properties['omegaL0'])+"\n")
+            f.write(str(sim['pos'].units.ratio(units.kpc,a=1)/1000.0 * sim.properties['h'])+"\n")
+            f.write(str(sim['vel'].units.ratio(units.km/units.s,a=1))+"\n")
+            f.write(str(sim['mass'].units.ratio(units.Msol))+"\n")
+            f.close()
+            # make input file
+            f = open('AHFstep.in','w')
+            f.write(sim._filename+" "+typecode+" 1\n")
+            f.write(sim._filename+"\n256\n5\n5\n0\n0\n0\n0\n")
+            f.close()
+        else:
+            # make input file
+            f = open('AHF.in','w')
+            f.write('[AHF]\n')
+            f.write('ic_filename = '+sim._filename+"\n")
+            f.write('ic_filetype = '+typecode+"\n")
+            f.write('outfile_prefix = '+sim._filename+"\n")
+            f.write('LgridDomain = 256\n')
+            lgridmax = int(2**np.ceil(np.log2(1.0 / np.min(sim['eps']))))
+            f.write('LgridMax = '+str(lgridmax)+'\n')
+            f.write('NperDomCell = 5\n')
+            f.write('NperRefCell = 5\n')
+            f.write('VescTune = 1.5\n')
+            f.write('NminPerHalo = 50\n')
+            f.write('RhoVir = 0\n')
+            f.write('Dvir = -1\n')
+            f.write('MaxGatherRad = 20.0\n\n')
+            f.write('[TIPSY]\n')
+            f.write('TIPSY_OMEGA0 = '+str(sim.properties['omegaM0'])+"\n")
+            f.write('TIPSY_LAMBDA0 = '+str(sim.properties['omegaL0'])+"\n")
+            f.write('TIPSY_BOXSIZE = '+str(sim['pos'].units.ratio(units.kpc,a=1)/1000.0 * sim.properties['h'])+"\n")
+            f.write('TIPSY_VUNIT = '+str(sim['vel'].units.ratio(units.km/units.s,a=1))+"\n")
+            f.write('TIPSY_MUNIT = '+str(sim['mass'].units.ratio(units.Msol))+"\n")
+            f.write('TIPSY_EUNIT = 0.03\n')
         f.close()
 
         if (not os.path.exists(sim._filename)):
             os.system("gunzip "+sim._filename+".gz")
         # determine parallel possibilities
-        # find AHFstep
-        for directory in os.environ["PATH"].split(os.pathsep) :
-            groupfinder = os.path.join(directory,"AHFstep")
-            if os.path.exists(groupfinder) :
-                # run it
-                os.system(groupfinder+" AHFstep.in")
-                return
+
+        if os.path.exists(groupfinder) :
+            # run it
+            os.system(groupfinder+" AHF.in")
+            return
 
     @staticmethod
     def _can_run(sim) :
         for directory in os.environ["PATH"].split(os.pathsep) :
-            if os.path.exists(os.path.join(directory,"AHFstep")) :
+            if (len(glob.glob(os.path.join(directory,"AHF*"))) > 0):
                 return True
         return False
 
