bd6f135e8d87ddd1cf2b4600fa3d5256215e04c9
Andrew Pontzen <andrew.pontzen@astro.ox.ac.uk>
2013-05-08 16:08:04 +0100

    Remove extraneous pyplot.show command

--- pynbody/plot/sph.py
+++ pynbody/plot/sph.py
@@ -231,7 +231,8 @@ def image(sim, qty='rho', width=10, resolution=500, units=None, log=True,
         
     
     plt.draw()
-    plt.show()
+    # plt.show() - removed by AP on 30/01/2013 - this should not be here as
+    #              for some systems you don't get back to the command prompt
 
     return im
 
