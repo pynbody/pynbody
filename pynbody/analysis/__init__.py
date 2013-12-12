7db4f01d4a33248bc9695db90f262a0d2d8242ae
Rok Roskar <roskar@physik.uzh.ch>
2013-12-12 13:50:35 +0100

    remove m_exan until the bugs are dealt with

--- pynbody/analysis/__init__.py
+++ pynbody/analysis/__init__.py
@@ -1,7 +1,6 @@
 import imp
 from . import profile,  cosmology, halo, luminosity, hmf
 from . import ionfrac, pkdgrav_cosmo
-from . import m_exan
 
 from .decomp import decomp
 from .hmf import halo_mass_function
@@ -16,6 +15,5 @@ imp.reload(halo)
 imp.reload(luminosity)
 imp.reload(ionfrac)
 imp.reload(hmf)
-imp.reload(m_exan)
 
 import numpy as np
