66912984275164ed2dc52a5efdb1831d08059a9a
Alan Duffy <mail@alanrduffy.com>
2014-06-25 10:48:58 +1000

    Added a check for the Centre of Mass array. Currently it's showing a bug in subhalo def

--- nose/gadgethdf_test.py
+++ nose/gadgethdf_test.py
@@ -67,18 +67,33 @@ def test_halo_values() :
     exec code in module.__dict__
     pyread_gadget_hdf5 = module.pyread_gadget_hdf5    
 
-    FoF_Mass = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'Mass', sub_dir='fof', nopanda=True, silent=True, physunits=True)
-    FoF_MassType = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'MassType', sub_dir='fof', nopanda=True, silent=True, physunits=True)
-    Sub_Mass = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'Mass', sub_dir='subfind', nopanda=True, silent=True, physunits=True)
-    Sub_MassType = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'MassType', sub_dir='subfind', nopanda=True, silent=True, physunits=True)
-    NsubPerHalo = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'NsubPerHalo', sub_dir='subfind', nopanda=True, silent=True, physunits=True)
+    FoF_Mass = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'Mass', sub_dir='fof', nopanda=True, silent=True) * 1e10
+    FoF_MassType = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'MassType', sub_dir='fof', nopanda=True, silent=True) * 1e10
+    Sub_Mass = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'Mass', sub_dir='subfind', nopanda=True, silent=True) * 1e10
+    Sub_MassType = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'MassType', sub_dir='subfind', nopanda=True, silent=True) * 1e10
+    NsubPerHalo = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'NsubPerHalo', sub_dir='subfind', nopanda=True, silent=True)
     OffsetHalo = np.roll(NsubPerHalo.cumsum(), 1)
     OffsetHalo[0]=0 ## To start counter
 
-    h = subfind.halos()
     subfind.physical_units()
+    h = subfind.halos() ## Need everything in Mpc / Msol
 
-    # Test the total mass of each component for FOF halos
+    FoF_CoM = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'CenterOfMass', sub_dir='fof', nopanda=True, silent=True)
+    Sub_CoM = pyread_gadget_hdf5(filesub+'.0.hdf5', 10, 'CenterOfMass', sub_dir='subfind', nopanda=True, silent=True)
+
+    # Check the Halo Array values
+    for i,halo in enumerate(h[0:10]) : 
+        if np.allclose(halo.properties['CenterOfMass'], FoF_CoM[i], rtol=1e-3) == False:
+	    	print halo.properties['CenterOfMass'], FoF_CoM[i]
+        for j, s in enumerate(halo.sub) : 
+	        if np.allclose(s.properties['CenterOfMass'], Sub_CoM[OffsetHalo[i]+j], rtol=1e-3) == False:
+		    	print s.properties['CenterOfMass'], i, OffsetHalo[i], j, Sub_CoM[OffsetHalo[i]+j]
+
+    ###
+    # Test the Halo particle information
+    ###
+
+    # Mass of each component for FOF halos
     for i,halo in enumerate(h[0:10]) : 
         if np.allclose(halo.g['mass'].sum(), FoF_MassType[i,0], rtol=1e-3) == False:
             print 'FoF Gas mass failed ', halo.g['mass'].sum(), FoF_MassType[i,0]
@@ -89,7 +104,7 @@ def test_halo_values() :
         if np.allclose(halo['mass'].sum(), FoF_Mass[i], rtol=1e-3) == False:
             print 'FoF Total mass failed ', halo['mass'].sum(), FoF_Mass[i]
 
-    # Testing masses in SubHaloes
+    # Mass of each component for Subhalos
     for i,halo in enumerate(h[0:10]) : 
         for j, s in enumerate(halo.sub) : 
             if np.allclose(s.g['mass'].sum(), Sub_MassType[OffsetHalo[i]+j,0], rtol=1e-3) == False:
@@ -105,7 +120,7 @@ def test_halo_values() :
     FoF_Length = pyread_gadget_hdf5(filesub+'.0.hdf5', 0, 'Length', sub_dir='fof', nopanda=True, silent=True, physunits=True)
     FoF_Offset = pyread_gadget_hdf5(filesub+'.0.hdf5', 0, 'Offset', sub_dir='fof', nopanda=True, silent=True, physunits=True)
 
-    # Test the Particle Temperature 
+    # Test the Particle Temperature and implicitly the particle ordering
     for i,halo in enumerate(h[0:10]) : 
         if np.allclose(list(halo.g['temp']), list(chain.from_iterable(FoF_Temp[np.arange(FoF_Offset[i],FoF_Offset[i]+FoF_Length[i],dtype=np.int64)])), rtol=1e-3) == False:
             print "Gas Temperature failed", i,FoF_Offset[i],FoF_Length[i]
