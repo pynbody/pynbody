fcfebc6442f19c9a5621ad72447a7026d8d69bd7
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2014-07-27 16:22:40 +0100

    Fix failing openmp calls

--- pynbody/openmp/openmp_real.pyx
+++ pynbody/openmp/openmp_real.pyx
@@ -1,12 +1,19 @@
+import sys
 
-cimport openmp
+cdef extern from "omp.h" :
+     void omp_set_num_threads(int)
 
+cdef extern from "omp.h" : 
+     int omp_get_max_threads()
 
-def get_threads() :
-    return openmp.omp_get_max_threads()
+cdef extern from "omp.h" : 
+     int omp_get_num_procs()
 
-def set_threads(num) :
-    openmp.omp_set_num_threads(num)
+def get_threads() : 
+    return omp_get_max_threads()
 
-def get_cpus() :
-    return openmp.omp_get_num_procs()
+def set_threads(num) : 
+    omp_set_num_threads(num)
+
+def get_cpus() : 
+    return omp_get_num_procs()
