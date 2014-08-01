4641a87a417d835d5c663ca7686c4fceda18397c
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2014-08-01 15:51:55 +0100

    Adopt adaptable fused types across sph and bridge modules

--- pynbody/bridge/_bridge.pyx
+++ pynbody/bridge/_bridge.pyx
@@ -4,11 +4,28 @@ cimport cython
 
 from cython cimport integral
 
+
+# The following slightly odd repetitiveness is to force Cython to generate
+# code for different permutations of the possible integer inputs.
+#
+# Using just one fused type requires the types to be synchronized across all
+# inputs.
+
+ctypedef fused integral_1:
+    npc.int32_t
+    npc.int64_t
+
+ctypedef fused integral_2:
+    npc.int32_t
+    npc.int64_t
+
+
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def bridge(npc.ndarray[integral, ndim=1] iord_to,
-            npc.ndarray[integral, ndim=1] iord_from) :
-    
+def bridge(npc.ndarray[integral_1, ndim=1] iord_to,
+            npc.ndarray[integral_2, ndim=1] iord_from) :
+
     cdef npc.ndarray[npc.int64_t, ndim=1] output_index
     cdef int i=0, i_to=0, j=0
     cdef int length = len(iord_from)
@@ -16,7 +33,7 @@ def bridge(npc.ndarray[integral, ndim=1] iord_to,
 
 
     output_index = np.empty(length_to,dtype=np.int64)
-    
+
     for i in range(length) :
         i_from = iord_from[i]
         while i_from>iord_to[i_to] and i_to<length_to :
@@ -24,27 +41,26 @@ def bridge(npc.ndarray[integral, ndim=1] iord_to,
         if i_from==iord_to[i_to] :
             output_index[j] = i_to
             j+=1
-            
+
     return output_index[:j]
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def match(npc.ndarray[integral, ndim=1] group_list_1,
-          npc.ndarray[integral, ndim=1] group_list_2,
+def match(npc.ndarray[integral_1, ndim=1] group_list_1,
+          npc.ndarray[integral_2, ndim=1] group_list_2,
           int imin, int imax):
     cdef int i, l = len(group_list_1)
     cdef int g1, g2
     cdef npc.ndarray[npc.int64_t, ndim=2] output = np.zeros((imax+1-imin,imax+1-imin),dtype=np.int64)
-    
+
     assert len(group_list_2)==l
-    
+
     with nogil:
         for i in range(l):
             g1 = group_list_1[i]
             g2 = group_list_2[i]
             if g1<=imax and g2<=imax and g1>=imin and g2>=imin :
                 output[g1-imin,g2-imin]+=1
-                
+
     return output
-            
--- pynbody/sph/_render.pyx
+++ pynbody/sph/_render.pyx
@@ -6,25 +6,55 @@ cimport libc.math as cmath
 from libc.math cimport atan, pow
 from libc.stdlib cimport malloc, free
 
+# The following slightly odd repetitiveness is to force Cython to generate
+# code for different permutations of the possible integer inputs.
+#
+# Using just one fused type requires the types to be consistent across all
+# arguments.
+
+ctypedef fused fused_input_type_1:
+    np.float32_t
+    np.float64_t
+
+ctypedef fused fused_input_type_2:
+    np.float32_t
+    np.float64_t
+
+ctypedef fused fused_input_type_3:
+    np.float32_t
+    np.float64_t
+
+ctypedef fused fused_input_type_4:
+    np.float32_t
+    np.float64_t
+
+ctypedef fused fused_input_type_5:
+    np.float32_t
+    np.float64_t
+
+
 ctypedef np.float32_t image_output_type
 np_image_output_type = np.float32
-ctypedef np.float64_t input_quantities_type
 
-        
+ctypedef np.float64_t fixed_input_type
+
+
+
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-def render_spherical_image_core(np.ndarray[input_quantities_type, ndim=1] rho, # array of particle densities
-                                np.ndarray[input_quantities_type, ndim=1] mass, # array of particle masses
-                                np.ndarray[input_quantities_type, ndim=1] qtyar, # array of quantity to make image of
-                                np.ndarray[input_quantities_type, ndim=2] pos, # array of particle positions
-                                np.ndarray[input_quantities_type, ndim=1] r, # particle radius
-                                np.ndarray[input_quantities_type, ndim=1] h, # particle smoothing length
+def render_spherical_image_core(np.ndarray[fused_input_type_1, ndim=1] rho, # array of particle densities
+                                np.ndarray[fused_input_type_2, ndim=1] mass, # array of particle masses
+                                np.ndarray[fused_input_type_3, ndim=1] qtyar, # array of quantity to make image of
+                                np.ndarray[fused_input_type_4, ndim=2] pos, # array of particle positions
+                                np.ndarray[fused_input_type_4, ndim=1] r, # particle radius
+                                np.ndarray[fused_input_type_4, ndim=1] h, # particle smoothing length
                                 np.ndarray[np.int64_t, ndim=1] ind, # which of the above particles to use
-                                np.ndarray[input_quantities_type, ndim=1] ds, # what distances to sample at (in units of smoothing)
-                                np.ndarray[input_quantities_type, ndim=1] weights, # what kernel weighting to use at these samples
+                                np.ndarray[fused_input_type_5, ndim=1] ds, # what distances to sample at (in units of smoothing)
+                                np.ndarray[fused_input_type_5, ndim=1] weights, # what kernel weighting to use at these samples
                                 unsigned int nside) :
-                                
+
     cdef unsigned int i,i0,j,n=len(ind),m=len(ds),num_pix
     cdef long k
     cdef float angle, norm, den
@@ -37,7 +67,7 @@ def render_spherical_image_core(np.ndarray[input_quantities_type, ndim=1] rho, #
     im = np.zeros(hp.nside2npix(nside), dtype=np_image_output_type)
     im_norm = np.zeros_like(im)
     buff = np.empty(len(im), dtype=np.int64)
-    
+
     if not hp.isnsideok(nside):
         raise ValueError('Wrong nside value, must be a power of 2')
 
@@ -61,7 +91,7 @@ def render_spherical_image_core(np.ndarray[input_quantities_type, ndim=1] rho, #
                     im[healpix_pixels[k]]+=den
                     im_norm[healpix_pixels[k]]+=norm
 
-                
+
     return im, im_norm
 
 
@@ -69,21 +99,21 @@ def render_spherical_image_core(np.ndarray[input_quantities_type, ndim=1] rho, #
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-cdef image_output_type get_kernel(input_quantities_type d2, input_quantities_type kernel_max_2,
-                                  image_output_type h_to_the_kdim, int num_samples, 
+cdef image_output_type get_kernel(fixed_input_type d2, fixed_input_type kernel_max_2,
+                                  image_output_type h_to_the_kdim, int num_samples,
                                   image_output_type* kvals) nogil :
     cdef unsigned int index = <unsigned int>(num_samples*(d2/kernel_max_2))
     if index<num_samples :
         return kvals[index]/h_to_the_kdim
     else :
         return 0
-    
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-cdef image_output_type get_kernel_xyz(input_quantities_type x, input_quantities_type y, input_quantities_type z, input_quantities_type kernel_max_2,
-                                       image_output_type h_to_the_kdim, int num_samples, 
+cdef image_output_type get_kernel_xyz(fixed_input_type x, fixed_input_type y, fixed_input_type z, fixed_input_type kernel_max_2,
+                                       image_output_type h_to_the_kdim, int num_samples,
                                  image_output_type* kvals) nogil :
      return get_kernel(x*x+y*y+z*z,kernel_max_2,h_to_the_kdim,num_samples,kvals)
 
@@ -93,21 +123,26 @@ cdef image_output_type get_kernel_xyz(input_quantities_type x, input_quantities_
 @cython.wraparound(False)
 @cython.cdivision(True)
 def render_image(int nx, int ny,
-                 np.ndarray[input_quantities_type,ndim=1] x, np.ndarray[input_quantities_type,ndim=1] y, np.ndarray[input_quantities_type,ndim=1] z,
-                 np.ndarray[input_quantities_type,ndim=1] sm,
-                 input_quantities_type x1,input_quantities_type x2,input_quantities_type y1,input_quantities_type y2,input_quantities_type z_camera,float z0,
-                 np.ndarray[input_quantities_type,ndim=1] qty, np.ndarray[input_quantities_type,ndim=1] mass, np.ndarray[input_quantities_type,ndim=1] rho,
-                 input_quantities_type smooth_lo, input_quantities_type smooth_hi,
+                 np.ndarray[fused_input_type_1,ndim=1] x,
+                 np.ndarray[fused_input_type_1,ndim=1] y,
+                 np.ndarray[fused_input_type_1,ndim=1] z,
+                 np.ndarray[fused_input_type_2,ndim=1] sm,
+                 fixed_input_type x1,fixed_input_type x2,fixed_input_type y1,
+                 fixed_input_type y2,fixed_input_type z_camera, fixed_input_type z0,
+                 np.ndarray[fused_input_type_3,ndim=1] qty,
+                 np.ndarray[fused_input_type_4,ndim=1] mass,
+                 np.ndarray[fused_input_type_5,ndim=1] rho,
+                 fixed_input_type smooth_lo, fixed_input_type smooth_hi,
                  kernel) :
-    
-    cdef input_quantities_type pixel_dx = (x2-x1)/nx
-    cdef input_quantities_type pixel_dy = (y2-y1)/ny
-    cdef input_quantities_type x_start = x1+pixel_dx/2
-    cdef input_quantities_type y_start = y1+pixel_dy/2
+
+    cdef fixed_input_type pixel_dx = (x2-x1)/nx
+    cdef fixed_input_type pixel_dy = (y2-y1)/ny
+    cdef fixed_input_type x_start = x1+pixel_dx/2
+    cdef fixed_input_type y_start = y1+pixel_dy/2
     cdef int n_part = len(x)
     cdef int nn=0, i=0
-    cdef input_quantities_type x_i, y_i, z_i, sm_i, qty_i
-    cdef input_quantities_type x_pixel, y_pixel, z_pixel
+    cdef fixed_input_type x_i, y_i, z_i, sm_i, qty_i
+    cdef fixed_input_type x_pixel, y_pixel, z_pixel
     cdef int x_pos, y_pos
     cdef int x_pix_start, x_pix_stop, y_pix_start, y_pix_stop
 
@@ -118,20 +153,20 @@ def render_image(int nx, int ny,
     cdef float mid_y = (y2+y1)/2
     cdef float dz_i
 
-    
+
     cdef int kernel_dim = kernel.h_power
-    cdef input_quantities_type max_d_over_h = kernel.max_d
+    cdef fixed_input_type max_d_over_h = kernel.max_d
 
 
     cdef np.ndarray[image_output_type,ndim=1] samples = kernel.get_samples(dtype=np_image_output_type)
     cdef int num_samples = len(samples)
     cdef image_output_type* samples_c = <image_output_type*>samples.data
     cdef image_output_type sm_to_kdim   # minimize casting when same type as output
-   
-    cdef input_quantities_type kernel_max_2 # minimize casting when same type as input
-    
+
+    cdef fixed_input_type kernel_max_2 # minimize casting when same type as input
+
     cdef np.ndarray[image_output_type,ndim=2] result = np.zeros((nx,ny),dtype=np_image_output_type)
-    
+
     z_pixel = z0
     cdef int total_ptcls = 0
 
@@ -160,8 +195,8 @@ def render_image(int nx, int ny,
                 pixel_dy = (y2-y1)/ny
                 x_start = x1+pixel_dx/2
                 y_start = y1+pixel_dy/2
-              
-            
+
+
             # check particle smoothing is within specified range
             if sm_i<pixel_dx*smooth_lo or sm_i>pixel_dx*smooth_hi : continue
 
@@ -180,17 +215,17 @@ def render_image(int nx, int ny,
                 # only 2, 3 supported
 
             kernel_max_2 = (sm_i*sm_i)*(max_d_over_h*max_d_over_h)
-                
+
             # decide whether this is a single pixel or a multi-pixel particle
             if (max_d_over_h*sm_i/pixel_dx<1 and max_d_over_h*sm_i/pixel_dy<1) :
                 # single pixel, get pixel location
                 x_pos = int((x_i-x1)/pixel_dx)
                 y_pos = int((y_i-y1)/pixel_dy)
-            
+
                 # work out pixel centre
-                x_pixel = (pixel_dx*<input_quantities_type>(x_pos)+x_start)
-                y_pixel = (pixel_dy*<input_quantities_type>(y_pos)+y_start)
-                
+                x_pixel = (pixel_dx*<fixed_input_type>(x_pos)+x_start)
+                y_pixel = (pixel_dy*<fixed_input_type>(y_pos)+y_start)
+
                 # final bounds check
                 if x_pos>=0 and x_pos<nx and y_pos>=0 and y_pos<ny :
                     result[y_pos,x_pos]+=qty_i*get_kernel_xyz(x_i-x_pixel, y_i-y_pixel, (z_i-z_pixel)*use_z, kernel_max_2 ,sm_to_kdim,num_samples,samples_c)
@@ -205,10 +240,10 @@ def render_image(int nx, int ny,
                 if y_pix_start<0 : y_pix_start = 0
                 if y_pix_stop>ny : y_pix_stop = ny
                 for x_pos in range(x_pix_start, x_pix_stop) :
-                    x_pixel = pixel_dx*<input_quantities_type>(x_pos)+x_start
+                    x_pixel = pixel_dx*<fixed_input_type>(x_pos)+x_start
                     for y_pos in range(y_pix_start, y_pix_stop) :
-                        y_pixel = pixel_dy*<input_quantities_type>(y_pos)+y_start
-                        
+                        y_pixel = pixel_dy*<fixed_input_type>(y_pos)+y_start
+
                         # could accessing the buffer manually be
                         # faster? It seems to be FAR faster (x10!) but
                         # only when using stack-allocated memory for
@@ -220,13 +255,9 @@ def render_image(int nx, int ny,
                         # disappears from the compiled version in the
                         # instance described above.  Anyway for now, there
                         # is no advantage to the manual approach -
-                        
+
                         #c_result[x_pos+nx*y_pos]+=qty_i*get_kernel_xyz(x_i-x_pixel, y_i-y_pixel, (z_i-z_pixel)*use_z, kernel_max_2 ,sm_to_kdim,num_samples,samples_c)
 
                         result[y_pos,x_pos]+=qty_i*get_kernel_xyz(x_i-x_pixel, y_i-y_pixel, (z_i-z_pixel)*use_z, kernel_max_2 ,sm_to_kdim,num_samples,samples_c)
 
     return result
-                        
-
-
-                    
