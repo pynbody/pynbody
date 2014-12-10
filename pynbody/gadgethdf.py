Corrected Halpha units

pynbody/gadgethdf.py
@@ -552,7 +552,6 @@ def em(sim) :
 
     return sim.g["rho_ne"]*sim.g["rho_ne"]
 
-
 @GadgetHDFSnap.derived_quantity
 @SubFindHDFSnap.derived_quantity
 def halpha(sim) :
@@ -563,9 +562,9 @@ def halpha(sim) :
     #alpha = 2.54e-13 * (sim.g['temp'].in_units('K') / 1e4)**(-0.8163-0.0208*np.log(sim.g['temp'].in_units('K') / 1e4))
     #alpha.units = units.cm**(3) * units.s**(-1)
 
-    ## http://astro.berkeley.edu/~ay216/08/NOTES/Lecture08-08.pdf
+    ## H alpha intensity = (h Nu(Halpha)/4Pi) * 7.864e-14 T_1e4K from http://astro.berkeley.edu/~ay216/08/NOTES/Lecture08-08.pdf
     alpha = (6.6260755e-27) * (6562.81) * 7.864e-14 * (1e4 / sim.g['temp'].in_units('K')) / (4.*np.pi)
-    alpha.units = (units.erg * units.s) * (units.angst) * units.cm**(3) * units.s**(-1)
+    alpha.units = units.erg * units.cm**(3) * units.s**(-1) ## It's intensity
 
     return alpha * sim["em"]
 