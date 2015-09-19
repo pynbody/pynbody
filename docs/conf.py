d64a4af3e17b71ad762f4b9c718ca4fa2c4e7c37
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2015-09-19 12:54:06 +0100

    Fix errors in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -43,13 +43,6 @@ extensions = ['sphinx.ext.autodoc',
               'numpydoc']
 
 
-"""
-# IN PRINCIPLE,
-# newer versions of Ipython include the directive, older versions need our
-# own copy.
-#
-# HOWEVER, there is a big bug in the shipped ipython_directive, so we
-# continue to use our own patched copy.
 
 try:
     import IPython.sphinxext.ipython_console_highlighting
@@ -57,9 +50,6 @@ try:
                  'IPython.sphinxext.ipython_directive']
 except ImportError:
     extensions+=['ipython_console_highlighting','ipython_directive']
-"""
-
-extensions+=['ipython_console_highlighting','ipython_directive']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
