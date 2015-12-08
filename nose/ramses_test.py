e53f89024f2b8ab3eecc1aa72788ff05d94dedb5
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2015-12-08 12:54:35 +0000

    Add test for Ramses-RT loader

--- nose/ramses_test.py
+++ nose/ramses_test.py
@@ -49,3 +49,22 @@ def test_mass_unit_sanity():
     f2['mass']
 
     np.testing.assert_allclose(f1.dm['mass'], f2.dm['mass'], atol=1e-5)
+
+def test_rt_arrays():
+    f1 = pynbody.load("testdata/ramses_rt_partial_output_00002",cpus=[1,2,3])
+
+    for group in range(4):
+        assert 'rad_%d_rho'%group in f1.gas.loadable_keys()
+        assert 'rad_%d_flux'%group in f1.gas.loadable_keys()
+
+    f1.gas['rad_0_flux'] # ensure 3d name triggers loading
+
+    np.testing.assert_allclose(f1.gas['rad_0_rho'][::5000],
+      [  8.63987256e-02,   3.73498855e-04,   3.46061505e-04,
+         2.13979002e-04,   3.22825503e-04,   3.29494226e-04,
+         2.26216739e-04,   3.30639509e-06,   3.61922553e-05,
+         8.25142141e-06,   1.25595394e-05,   7.11374568e-07,
+         3.89547811e-05,   5.17086871e-06,   6.90921297e-08,
+         1.59989054e-09,   7.61815782e-07,   7.09372161e-08,
+         7.76265288e-09,   4.32642383e-09])
+
