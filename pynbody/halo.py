87a98b7f37548398ff9b8e0655baf4e3944c57cc
mtremmel <m.tremmel6@gmail.com>
2016-07-27 13:59:29 -0700

    make dosort False

--- pynbody/halo.py
+++ pynbody/halo.py
@@ -799,7 +799,7 @@ class AHFCatalogue(HaloCatalogue):
     """
 
     def __init__(self, sim, make_grp=None, get_all_parts=None, use_iord=None, ahf_basename=None,
-                 dosort=True, only_stat=None, write_fpos=True, **kwargs):
+                 dosort=False, only_stat=None, write_fpos=True, **kwargs):
         """Initialize an AHFCatalogue.
 
         **kwargs** :
