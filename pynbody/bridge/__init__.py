816c109b0ff8bfeda6e0eb75c7e95bf1a177ea50
Michael Tremmel <m.tremmel6@gmail.com>
2016-10-19 03:50:39 -0700

    Update AHF reader to deal more efficiently with bigger outputs (#341)

--- pynbody/bridge/__init__.py
+++ pynbody/bridge/__init__.py
@@ -171,8 +171,13 @@ class Bridge(object):
 
         else:
 
-            g1 = groups_1.get_fam_group_array(family=only_family)
-            g2 = groups_2.get_fam_group_array(family=only_family)
+            g1 = groups_1.get_group_array(family=only_family)
+            g2 = groups_2.get_group_array(family=only_family)
+
+        if max_index is None:
+            max_index = max(g1.max(),g2.max())
+        if min_index is None:
+            min_index = min(g1.min(),g2.min())
 
         transfer_matrix = _bridge.match(g1, g2, min_index, max_index)
 
