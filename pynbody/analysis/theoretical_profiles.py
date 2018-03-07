77d5e6b2a5fec227ef944389ec775446e0cb0248
Martin-Rey <mrey@imac7.star.ucl.ac.uk>
2018-03-07 16:47:57 +0000

    Untested but broadly working NFW

--- pynbody/analysis/theoretical_profiles.py
+++ pynbody/analysis/theoretical_profiles.py
@@ -15,11 +15,7 @@ import abc
 class AbstractBaseProfile:
 
     def __init__(self):
-        self._minimal_radius = 0
-        self._maximal_radius = np.inf
-
-        "Profile definition can depend on cosmology"
-        self.cosmological_context = None
+        pass
 
     @abc.abstractclassmethod
     def profile_functional(self, radius):
@@ -54,41 +50,33 @@ class AbstractBaseProfile:
 
 class NFWprofile(AbstractBaseProfile):
 
-    def __init__(self, context, scale_radius=None, central_density=None, concentration=None,
-                 virial_mass=None):
+    def __init__(self, halo_radius, scale_radius=None, central_density=None, concentration=None,
+                 halo_mass=None):
 
         super(AbstractBaseProfile, self).__init__()
 
+        self._halo_radius = halo_radius
+
         if scale_radius is None or central_density is None:
-            if concentration is None or virial_mass is None:
+            if concentration is None or halo_mass is None or halo_radius is None:
                 raise ValueError("You must provide concentration, virial mass"
-                                 "if not providing the central density and scale_radius")
+                                 " if not providing the central density and scale_radius")
             else:
                 self.concentration = concentration
-                self._virial_mass = virial_mass
-
-                self.cosmological_context = context
+                self._halo_mass = halo_mass
 
                 self.scale_radius = self._derive_scale_radius()
                 self.central_density = self._derive_central_overdensity()
 
         else:
-            if concentration is not None or virial_mass is not None:
+            if concentration is not None or halo_mass is not None:
                 raise ValueError("You can't provide both scale_radius+central_overdensity and concentration")
 
             self.scale_radius = scale_radius
             self.central_density = central_density
 
-            self.cosmological_context = context
             self.concentration = self._derive_concentration()
-            self._virial_mass = self.get_enclosed_value(self.scale_radius)
-
-        self._redshift = self.cosmological_context.properties['z']
-
-        # For now, this assumes virial mass against rho_crit
-        self._virial_radius = (self._virial_mass / cosmology.rho_crit(self.cosmological_context)) ** (1/3)
-
-
+            self._halo_mass = self.get_enclosed_value(self.scale_radius)
 
     @staticmethod
     def profile_functional_static(radius, central_density, scale_radius):
@@ -98,19 +86,18 @@ class NFWprofile(AbstractBaseProfile):
         return NFWprofile.profile_functional_static(radius, self.central_density, self.scale_radius)
 
     def _derive_concentration(self):
-        return self._virial_radius / self.scale_radius
+        return self._halo_radius / self.scale_radius
 
     def _derive_scale_radius(self):
-        return self._virial_radius / self.concentration
+        return self._halo_radius / self.concentration
 
     def _derive_central_overdensity(self):
-        return self._virial_mass / (4 * np.pi * self.scale_radius ** 3 *
-                                    NFWprofile._helper_function(self.concentration))
+        return self._halo_mass / (4 * np.pi * self.scale_radius ** 3 *
+                                  NFWprofile._helper_function(self.concentration))
 
     def get_enclosed_value(self, radius_of_enclosure):
         # Eq 7.139
-        return 4 * np.pi * self.scale_radius ** 3 * NFWprofile._helper_function(self.concentration
-                                                                                * radius_of_enclosure / self.scale_radius)
+        return 4 * np.pi * self.scale_radius ** 3 * NFWprofile._helper_function(self.concentration * radius_of_enclosure / self.scale_radius)
 
     def get_dlogrho_dlogr(self):
         # Theoretical slope
