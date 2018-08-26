b918d444442a14d9e8407a1661d49805d9e719f3
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2018-08-26 12:22:31 +0100

    Fix bug in IndexedSubSnap, where an entire family array of the parent SimSnap would be returned if len(subsnap.family)==0

--- pynbody/snapshot/__init__.py
+++ pynbody/snapshot/__init__.py
@@ -1885,7 +1885,7 @@ class IndexedSubSnap(SubSnap):
         return SimSnap._get_family_slice(self, fam)
 
     def _get_family_array(self, name, fam, index=None, always_writable=False):
-        sl = self._family_indices.get(fam,None)
+        sl = self._family_indices.get(fam,slice(0,0))
         sl = util.concatenate_indexing(sl, index)
 
         return self.base._get_family_array(name, fam, sl, always_writable)
@@ -2003,7 +2003,7 @@ def new(n_particles=0, order=None, **families):
     f = new(dm=50, star=25, gas=25, order='star,gas,dm')
 
     guarantees the stars, then gas, then dark matter particles appear
-    in sqeuence.
+    in sequence.
     """
 
     if len(families) == 0:
