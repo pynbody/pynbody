a89fe8c1ccd5257539472311bbec06273ef81648
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2018-08-26 12:22:31 +0100

    Fix bug in IndexedSubSnap, where an entire family array of the parent SimSnap would be returned if len(subsnap.family)==0

--- nose/family_test.py
+++ nose/family_test.py
@@ -11,3 +11,15 @@ def test_family_array_dtype() :
     f = pynbody.load('testdata/g15784.lr.01024.gz')
     f.g['rho'] = np.zeros(len(f.g),dtype=np.float32)
     f.s['rho']
+
+def test_family_array_null_slice():
+    """Regression test for issue where getting a family array for an IndexedSubSnap containing no members of that family
+    - would erroneously return the entire family array"""
+
+    test = pynbody.new(dm=10, star=10, order='dm,star')
+    test.star['TestFamilyArray'] = 1.0
+    assert len(test[[1,3,5,7]].star)==0 # this always succeeded
+    assert len(test[[1,3,5,7]].star['mass'])==0 # this always succeeded
+    assert len(test[1:9:2].star['TestFamilyArray'])==0 # this always succeeded
+    assert len(test[[1, 3, 5, 11,13]].star['TestFamilyArray']) == 2  # this always succeeded
+    assert len(test[[1,3,5,7]].star['TestFamilyArray'])==0 # this would fail
