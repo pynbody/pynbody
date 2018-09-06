22ad3ad44e725a4bfe1560d913e8f6dc42fb551b
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2018-09-06 09:34:42 +0200

    Update/simplify documentation

--- docs/conf.py
+++ docs/conf.py
@@ -115,10 +115,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+html_theme = 'alabaster'
 
 # set up bootstrap
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+"""html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 html_theme_options = {
     'bootswatch_theme': 'spacelab',
@@ -126,7 +126,7 @@ html_theme_options = {
     'navbar_site_name': 'Sections',
 
     'source_link_position': 'footer'
-}
+}"""
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -145,7 +145,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "_static/logo.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
