e6a381d0267bdc015c05616f36ef09d63d8ae940
Corentin Cadiou <contact@cphyc.me>
2019-12-09 16:39:18 +0000

    Now reading tree correctly

--- nose/adaptahop_test.py
+++ nose/adaptahop_test.py
@@ -8,15 +8,13 @@ from pynbody.halo.adaptahop import AdaptaHOPCatalogue
 
 def test_load_adaptahop_catalogue():
     f = pynbody.load('/home/ccc/Documents/prog/yt-data/output_00080/info_00080.txt') 
-    h = AdaptaHOPCatalogue(f, '/home/ccc/tmp/tree_bricks099')
+    h = f.halos(read_contamination=True)
+    assert len(h) == 151
 
-    assert len(h) == 30875
-
-def test_load():
+def test_load_one_halo():
     f = pynbody.load('/home/ccc/Documents/prog/yt-data/output_00080/info_00080.txt') 
-    h = AdaptaHOPCatalogue(f, '/home/ccc/tmp/tree_bricks099')
-
-    h[1].properties
+    h = f.halos(read_contamination=True)
+    h[1]
 
 if __name__ == '__main__':
     f = pynbody.load('/home/ccc/Documents/prog/yt-data/output_00080/info_00080.txt') 
