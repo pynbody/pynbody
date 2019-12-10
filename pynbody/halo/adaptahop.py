d3761c2df1628451ae6079db189e8935c4f6b36c
Corentin Cadiou <contact@cphyc.me>
2019-12-10 16:56:10 +0000

    Support old/new format of AdaptaHOP
    This adds support to the old format (single precision) and add layout
    for new one.

--- pynbody/halo/adaptahop.py
+++ pynbody/halo/adaptahop.py
@@ -18,50 +18,24 @@ class DummyHalo(object):
 
 
 class AdaptaHOPCatalogue(HaloCatalogue):
+class BaseAdaptaHOPCatalogue(HaloCatalogue):
     """A AdaptaHOP Catalogue. AdaptaHOP output files must be in
     Halos/<simulation_number>/ directory or specified by fname"""
 
     _AdaptaHOP_fname = None
     _halos = None
-    _metadata = None
+    _headers = None
     _fname = ''
     _read_contamination = False
 
-    # List of the attributes read from file. This does *not* include the number of particles,
-    # the list of particles, the id of the halo and the "timestep" (first 4 records).
-    _halo_attributes = (
-        (('level', 'host_id', 'first_subhalo_id', 'n_subhalos', 'next_subhalo_id'), 5, 'i'),
-        ('m', 1, 'd'),
-        ('ntot', 1, 'i'),
-        ('mtot', 1, 'd'),
-        (('x', 'y', 'z'), 3, 'd'),
-        (('vx', 'vy', 'vz'), 3, 'd'),
-        (('lx', 'ly', 'lz'), 3, 'd'),
-        (('r', 'a', 'b', 'c'), 4, 'd'),
-        (('ek', 'ep', 'etot'), 3, 'd'),
-        ('spin', 1 , 'd'),
-        ('sigma', 1, 'd'),
-        (('rvir', 'mvir', 'Tvir', 'vvir'), 4, 'd'),
-        (('rmax', 'vmax'), 2, 'd'),
-        ('c', 1, 'd'),
-        (('r200', 'm200'), 2, 'd'),
-        (('r50', 'r90'), 2, 'd'),
-        ('rr3D', -1, 'd'),
-        ('rho3d', -1, 'd'),
-        (('rho0', 'R_c'), 2, 'd')
-    )
-
-    _halo_attributes_contam = (
-        ('contaminated', 1, 'i'),
-        (('m_contam', 'mtot_contam'), 2, 'd'),
-        (('n_contam', 'ntot_contam'), 2, 'i')
-    )
+    _halo_attributes = tuple()
+    _halo_attributes_contam = tuple()
+    _header_attributes = tuple()
 
     def __init__(self, sim, fname=None, read_contamination=False):
 
         if fname is None:
             for fname in AdaptaHOPCatalogue._enumerate_hop_tag_locations_from_sim(sim):
-                print('Trying {fname}'.format(fname=fname))
                 if os.path.exists(fname):
                     break
 
@@ -72,47 +46,42 @@ class AdaptaHOPCatalogue(HaloCatalogue):
         self._halos = {}
         self._fname = fname
         self._AdaptaHOP_fname = fname
-        self._metadata = {}
         self._read_contamination = read_contamination
 
         # Call parent class
-        super(AdaptaHOPCatalogue, self).__init__(sim)
+        super(BaseAdaptaHOPCatalogue, self).__init__(sim)
+
+        logger.debug("AdaptaHOPCatalogue loading offsets")
 
         # Compute offsets
         self._ahop_compute_offset()
+        logger.debug("AdaptaHOPCatalogue loaded")
 
     def _ahop_compute_offset(self):
         """
         Compute the offset in the brick file of each halo.
         """
         with FortranFile(self._fname) as fpu:
-            npart = fpu.read_vector('i')[0]
-            massp = fpu.read_vector('d')[0]
-            aexp = fpu.read_vector('d')[0]
-            omega_t = fpu.read_vector('d')[0]
-            age = fpu.read_vector('d')[0]
-            nhalos, nsubs = fpu.read_vector('i')
+            self._headers = fpu.read_attrs(self._header_attributes).copy()
 
-            self._metadata.update(
-                npart=npart, massp=massp, aexp=aexp, omega_t=omega_t, age=age, nhalos=nhalos,
-                nsubs=nsubs
-            )
+            nhalos = self._headers['nhalos']
+            nsubs = self._headers['nsubs']
+
+            Nskip = len(self._halo_attributes)
+            if self._read_halo_data:
+                Nskip += len(self._halo_attributes_contam)
 
             for _ in range(nhalos + nsubs):
                 ipos = fpu.tell()
                 fpu.skip(2)  # number + ids of parts
-                halo_ID = fpu.read_vector('i')[0]
-                fpu.skip(1)  # timesteps
+                halo_ID = fpu.read_int()
+                fpu.skip(Nskip)
 
-                # Skip data from halo
-                fpu.skip(len(self._halo_attributes))
-                if self._read_contamination:
-                    fpu.skip(len(self._halo_attributes_contam))
-
-                # Fill-in dummy container
+                # Fill-in data
                 dummy = DummyHalo()
                 dummy.properties['file_offset'] = ipos
                 self._halos[halo_ID] = dummy
+        print('Done!')
 
     def calc_item(self, halo_id):
         return self._get_halo(halo_id)
@@ -125,7 +94,7 @@ class AdaptaHOPCatalogue(HaloCatalogue):
         if not isinstance(halo, Halo):
             halo = self._halos[halo_id] = \
                 self._read_halo_data(halo_id, halo.properties['file_offset'])
-        return halo            
+        return halo
 
     def _read_halo_data(self, halo_id, offset):
         """
@@ -135,7 +104,7 @@ class AdaptaHOPCatalogue(HaloCatalogue):
         ----------
         halo_id : int
             The id of the halo
-        offset : int 
+        offset : int
             The location in the file (in bytes)
 
         Returns
@@ -149,22 +118,27 @@ class AdaptaHOPCatalogue(HaloCatalogue):
             index_array = fpu.read_vector('i')
             halo_id_read = fpu.read_int()
             assert halo_id == halo_id_read
-            fpu.skip(1)  # skip timestep
             if self._read_contamination:
                 attrs = self._halo_attributes + self._halo_attributes_contam
             else:
                 attrs = self._halo_attributes
-            parameters = fpu.read_attrs(attrs)
+            props = fpu.read_attrs(attrs)
+
+        # Convert positions between [-Lbox/2, Lbox/2] to [0, Lbox].
+        # /!\: AdaptaHOP assumes that 1Mpc == 3.08e24 (exactly)
+        boxsize = self._base().properties['boxsize']
+        Mpc2boxsize = boxsize.in_units('cm') / 3.08e24  # Hard-coded in AdaptaHOP...
+        for k in 'xyz':
+            props[k] = boxsize.in_units('Mpc') * (props[k] / Mpc2boxsize + 0.5)
 
         parameters['file_offset'] = offset
         parameters['npart'] = npart
 
         # Create halo object and fill properties
-        halo = Halo(halo_id, self, self.base, index_array)
-        halo.parameters.update(parameters)
+        halo = Halo(halo_id, self, self.base, np.abs(index_array))
+        halo.properties.update(props)
 
         return halo
-
 
     @staticmethod
     def _can_load(sim, arr_name='grp', *args, **kwa):
@@ -192,3 +166,72 @@ class AdaptaHOPCatalogue(HaloCatalogue):
 
     def _can_run(self, *args, **kwa):
         return False
+
+class AdaptaHOPCatalogue(BaseAdaptaHOPCatalogue):
+    _header_attributes = (
+        ('npart', 1, 'i'),
+        ('massp', 1, 'd'),
+        ('aexp', 1, 'd'),
+        ('omega_t', 1, 'd'),
+        ('age', 1, 'd'),
+        (('nhalos', 'nsubs'), 2, 'i')
+    )
+
+    # List of the attributes read from file. This does *not* include the number of particles,
+    # the list of particles, the id of the halo and the "timestep" (first 4 records).
+    _halo_attributes = (
+        ('timestep', 1 , 'i'),
+        (('level', 'host_id', 'first_subhalo_id', 'n_subhalos', 'next_subhalo_id'), 5, 'i'),
+        ('m', 1, 'd'),
+        ('ntot', 1, 'i'),
+        ('mtot', 1, 'd'),
+        (('x', 'y', 'z'), 3, 'd'),
+        (('vx', 'vy', 'vz'), 3, 'd'),
+        (('lx', 'ly', 'lz'), 3, 'd'),
+        (('r', 'a', 'b', 'c'), 4, 'd'),
+        (('ek', 'ep', 'etot'), 3, 'd'),
+        ('spin', 1 , 'd'),
+        ('sigma', 1, 'd'),
+        (('rvir', 'mvir', 'Tvir', 'vvir'), 4, 'd'),
+        (('rmax', 'vmax'), 2, 'd'),
+        ('c', 1, 'd'),
+        (('r200', 'm200'), 2, 'd'),
+        (('r50', 'r90'), 2, 'd'),
+        ('rr3D', -1, 'd'),
+        ('rho3d', -1, 'd'),
+        (('rho0', 'R_c'), 2, 'd')
+    )
+
+    _halo_attributes_contam = (
+        ('contaminated', 1, 'i'),
+        (('m_contam', 'mtot_contam'), 2, 'd'),
+        (('n_contam', 'ntot_contam'), 2, 'i')
+    )
+
+
+class OldAdaptaHOPCatalogue(BaseAdaptaHOPCatalogue):
+    _header_attributes = (
+        ('npart', 1, 'i'),
+        ('massp', 1, 'f'),
+        ('aexp', 1, 'f'),
+        ('omega_t', 1, 'f'),
+        ('age', 1, 'f'),
+        (('nhalos', 'nsubs'), 2, 'i')
+    )
+    # List of the attributes read from file. This does *not* include the number of particles,
+    # the list of particles, the id of the halo and the "timestep" (first 4 records).
+    _halo_attributes = (
+        ('timestep', 1 , 'i'),
+        (('level', 'host_id', 'first_subhalo_id', 'n_subhalos', 'next_subhalo_id'), 5, 'i'),
+        ('m', 1, 'f'),
+        (('x', 'y', 'z'), 3, 'f'),
+        (('vx', 'vy', 'vz'), 3, 'f'),
+        (('lx', 'ly', 'lz'), 3, 'f'),
+        (('r', 'a', 'b', 'c'), 4, 'f'),
+        (('ek', 'ep', 'etot'), 3, 'f'),
+        ('spin', 1 , 'f'),
+        (('rvir', 'mvir', 'Tvir', 'vvir'), 4, 'f'),
+        (('rho0', 'R_c'), 2, 'f')
+    )
+
+    _halo_attributes_contam = tuple()
