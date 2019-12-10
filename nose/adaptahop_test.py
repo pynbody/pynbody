83c5843d8f9b830cdc06595b67d1d9dd69000458
Corentin Cadiou <contact@cphyc.me>
2019-12-10 17:14:06 +0000

    Update tests

--- nose/adaptahop_test.py
+++ nose/adaptahop_test.py
@@ -7,15 +7,15 @@ import stat
 from pynbody.halo.adaptahop import AdaptaHOPCatalogue
 
 def test_load_adaptahop_catalogue():
-    f = pynbody.load('/home/ccc/Documents/prog/yt-data/output_00080/info_00080.txt') 
-    h = f.halos(read_contamination=True)
-    assert len(h) == 151
+    f = pynbody.load('/home/ccc/tmp/output_00099')
+    h = f.halos()
+    assert len(h) == h._headers['nhalos'] + h._headers['nsubs']
 
 def test_load_one_halo():
-    f = pynbody.load('/home/ccc/Documents/prog/yt-data/output_00080/info_00080.txt') 
-    h = f.halos(read_contamination=True)
+    f = pynbody.load('/home/ccc/tmp/output_00099')
+    h = f.halos()
     h[1]
 
 if __name__ == '__main__':
-    f = pynbody.load('/home/ccc/Documents/prog/yt-data/output_00080/info_00080.txt') 
-    h = AdaptaHOPCatalogue(f, '/home/ccc/tmp/tree_bricks099')
+    f = pynbody.load('/home/ccc/tmp/output_00099')
+    h = f.halos()
