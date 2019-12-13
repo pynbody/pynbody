2a21504e3b75581c49bf807240f83653b0e8873f
Corentin Cadiou <contact@cphyc.me>
2019-12-13 11:48:28 +0000

    Fix path to test data

--- nose/adaptahop_test.py
+++ nose/adaptahop_test.py
@@ -5,11 +5,11 @@ import numpy as np
 
 
 def test_load_adaptahop_catalogue():
-    f = pynbody.load('/home/ccc/tmp/output_00099')
+    f = pynbody.load('testdata/output_00080')
     h = f.halos()
     assert len(h) == h._headers['nhalos'] + h._headers['nsubs']
 
 def test_load_one_halo():
-    f = pynbody.load('/home/ccc/tmp/output_00099')
+    f = pynbody.load('testdata/output_00080')
     h = f.halos()
     np.testing.assert_allclose(h[1].properties['members'], h[1]['iord'])
