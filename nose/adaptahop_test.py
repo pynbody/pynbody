688dc7f51a1b2a3661ad3e024476859d42801b20
Corentin Cadiou <contact@cphyc.me>
2020-01-17 17:15:44 +0000

    Add corresponding test

--- nose/adaptahop_test.py
+++ nose/adaptahop_test.py
@@ -13,3 +13,19 @@ def test_load_one_halo():
     f = pynbody.load('testdata/output_00080')
     h = f.halos()
     np.testing.assert_allclose(h[1].properties['members'], h[1]['iord'])
+
+def test_get_group():
+    f = pynbody.load('testdata/output_00080')
+    h = f.halos()
+
+    group_array = h.get_group_array()
+    iord = f.dm['iord']
+
+    for halo_id in range(1, len(h)+1):
+        mask = group_array == halo_id
+
+        # Check that the indices
+        # - read from halo (h[halo_id]['iord'])
+        # - obtained from get_group_array masking
+        # are the same (in term of sets)
+        assert len(np.setdiff1d(iord[mask], h[halo_id]['iord'])) == 0
