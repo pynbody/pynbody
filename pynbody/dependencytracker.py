pynbody/pynbody

commit e74e7cbcf01449f7aba8b80fd1228fca4f0978b2

Fix bug #532 in ordering of locks in dependencytracker.py, which could lead to deadlock.
Add unit test

pynbody/dependencytracker.py
@@ -10,9 +10,9 @@ class DependencyContext(object):
         self.name = name
 
     def __enter__(self):
-        self.tracker._calculation_lock.__enter__()
         if self.name in self.tracker._current_calculation_stack:
             raise DependencyError, "Circular dependency"
+        self.tracker._calculation_lock.__enter__()
         self.tracker._push(self.name)
 
     def __exit__(self, exc_type, exc_val, exc_tb):

nose/sph_image_test.py
@@ -85,3 +85,12 @@ def test_render_stars():
     compare= np.load("test_stars_2d.npy")
 
     npt.assert_allclose(compare,im[40:60],atol=0.01)
+
+
+@pynbody.derived_array
+def intentional_circular_reference(sim):
+    return sim['intentional_circular_reference']
+
+def test_exception_propagation():
+    with npt.assert_raises(RuntimeError):
+        pynbody.plot.sph.image(f.gas, qty='intentional_circular_reference')

commit 8e9138ba07754a09d44bfaa85b92e86a925c2b84

Fix bug introduced by e74e7cb, which broke dependency tracker thread safety
The commit e74e7cb fixed a deadlock but also allowed the current set of calculations to mutate between
checking and entering the calculation lock. This caused an intermittent error in multi-threaded
code (especially image rendering).

pynbody/dependencytracker.py
@@ -10,9 +10,10 @@ class DependencyContext(object):
         self.name = name
 
     def __enter__(self):
-        if self.name in self.tracker._current_calculation_stack:
-            raise DependencyError, "Circular dependency"
         self.tracker._calculation_lock.__enter__()
+        if self.name in self.tracker._current_calculation_stack:
+            self.tracker._calculation_lock.__exit__(None, None, None)
+            raise DependencyError, "Circular dependency"
         self.tracker._push(self.name)
 
     def __exit__(self, exc_type, exc_val, exc_tb):