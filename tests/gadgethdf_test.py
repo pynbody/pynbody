Fix further test confused by introduction of units into properties dict

tests/gadgethdf_test.py
@@ -180,10 +180,10 @@ def test_hi_derivation():
 
 def test_fof_vs_sub_assignment():
     h = subfind.halos()
-    assert(np.allclose(h[0].properties['Mass'],28.604694074339932))
-    assert(np.allclose( h[0].properties['Halo_M_Crit200'], 29.796955896599684))
-    assert(np.allclose(h[1].properties['Mass'], 8.880245794949587))
-    assert(np.allclose(h[1].properties['Halo_M_Crit200'],8.116568749712314))
+    assert(np.allclose(h.get_halo_properties(0, with_unit=False)['Mass'],28.604694074339932))
+    assert(np.allclose(h.get_halo_properties(0, with_unit=False)['Halo_M_Crit200'], 29.796955896599684))
+    assert(np.allclose(h.get_halo_properties(1, with_unit=False)['Mass'], 8.880245794949587))
+    assert(np.allclose(h.get_halo_properties(1, with_unit=False)['Halo_M_Crit200'],8.116568749712314))
 
 def test_hdf_ordering():
     # HDF files do not intrinsically specify the order in which the particle types occur