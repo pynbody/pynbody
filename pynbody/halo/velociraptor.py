e137b58d15d58d999c2817b49711777b4dbcb9fe
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2024-01-01 20:20:37 +0000

    Improve velociraptor catalogue searching and parent/child relationships
    Add tests using a small sample file

--- pynbody/halo/velociraptor.py
+++ pynbody/halo/velociraptor.py
@@ -28,20 +28,22 @@ class VelociraptorCatalogue(HaloCatalogue):
         except ValueError:
             return None
 
-        halos_filename = f'catalogue_{snapshot_num:04d}/halos_{snapshot_num:04d}'
-
         possible_paths = [
-            simpath / halos_filename,
-            simpath / 'VR' / halos_filename,
-            simpath.parent / halos_filename,
-            simpath.parent / 'VR' / halos_filename,
+            simpath.parent,
+            simpath.parent / 'VR',
+            simpath.parent.parent,
+            simpath.parent.parent / 'VR'
         ]
 
-        for p in possible_paths:
-            if p.with_suffix('.properties.0').is_file():
-                return p
-
+        for basepath in possible_paths:
+            if basepath.is_dir():
+                for p in basepath.iterdir():
+                    if p.is_dir() and f'{snapshot_num:04d}' in p.name:
+                        possible_paths.append(p)
+                    if 'catalog_groups.0' in p.name and p.is_file():
+                        return basepath / str(p.name)[:-(len('.catalog_groups.0'))]
         return None
+
     @classmethod
     def _can_load(cls, sim, **kwargs):
         path = cls._catalogue_path(sim)
@@ -54,10 +56,16 @@ class VelociraptorCatalogue(HaloCatalogue):
 
         return True
 
-    def __init__(self, sim, include_unbound=False):
+    def __init__(self, sim, vr_basename=None, include_unbound=False):
         super().__init__(sim)
         self._include_unbound = include_unbound
-        self._path = self._catalogue_path(sim)
+        if vr_basename is None:
+            self._path = self._catalogue_path(sim)
+        else:
+            self._path = Path(vr_basename)
+
+        if self._path is None:
+            raise IOError("Could not find velociraptor catalogue. Try specifying vr_basename='path/to/output', where the velociraptor outputs are output.properties.0 etc")
         self._grps = h5py.File(str(self._path.with_suffix('.catalog_groups.0')), 'r')
         self._part_ids = h5py.File(str(self._path.with_suffix('.catalog_particles.0')), 'r')
         if include_unbound:
@@ -74,23 +82,31 @@ class VelociraptorCatalogue(HaloCatalogue):
         return self._num_halos
 
     def _calculate_children(self):
-        self._parents = self._grps['Parent_halo_ID'][:] - 1 # -1 to convert to zero-based indexing
-        _all_children = np.arange(self._num_halos, dtype=np.int32)[self._parents != -2]
-        self._all_children_ordered_by_parent = _all_children[np.argsort(self._parents[_all_children])]
-        self._children_start_index = np.searchsorted(self._parents[self._all_children_ordered_by_parent],
-                                                     np.arange(self._num_halos),
+        self._parents = self._grps['Parent_halo_ID'][:]
+        _all_children_zero_based = np.arange(self._num_halos, dtype=np.int32)[self._parents != -1]
+        self._all_children_ordered_by_parent = (
+                _all_children_zero_based[np.argsort(self._parents[_all_children_zero_based])]
+                + self._zero_offset
+        )
+
+        self._children_start_index = np.searchsorted(self._parents[self._all_children_ordered_by_parent-self._zero_offset],
+                                                     np.arange(self._num_halos+self._zero_offset),
                                                      side='left')
         self._children_stop_index = np.concatenate((self._children_start_index[1:],
                                                     np.array([self._num_halos],
                                                              dtype=self._children_start_index.dtype)))
 
     def _get_halo_properties(self, i):
-        i -= self._zero_offset
-        parent = self._parents[i]
+        i_zerobased = i - self._zero_offset
+        parent = self._parents[i_zerobased]
         children = self._all_children_ordered_by_parent[self._children_start_index[i]:self._children_stop_index[i]]
         return {'parent': parent, 'children': children}
+
     def _get_halo(self, i):
-        assert i<self._num_halos
+        if i >= self._num_halos+self._zero_offset or i < self._zero_offset:
+            raise IndexError(f"Halo index out of range (must be between {self._zero_offset}"
+                             f" and {self._num_halos+self._zero_offset-1})")
+
 
         ptcl_fpos = self._get_particle_offsets_for_halo(i)
         if self._include_unbound:
@@ -103,6 +119,7 @@ class VelociraptorCatalogue(HaloCatalogue):
         return h
 
     def _get_particle_offsets_for_halo(self, i, unbound=False):
+        i_zerobased = i - self._zero_offset
         if unbound:
             grps_hdf_array = self._grps['Offset_unbound']
             particle_ids_hdf_array = self._part_ids_unbound['Particle_IDs']
@@ -110,12 +127,12 @@ class VelociraptorCatalogue(HaloCatalogue):
             grps_hdf_array = self._grps['Offset']
             particle_ids_hdf_array = self._part_ids['Particle_IDs']
 
-        ptcl_start = grps_hdf_array[i]
+        ptcl_start = grps_hdf_array[i_zerobased]
 
-        if i == self._num_halos - 1:
+        if i_zerobased == self._num_halos - 1:
             ptcl_end = particle_ids_hdf_array.shape[0]
         else:
-            ptcl_end = grps_hdf_array[i + 1]
+            ptcl_end = grps_hdf_array[i_zerobased + 1]
 
         ptcl_ids_this_halo = particle_ids_hdf_array[ptcl_start:ptcl_end]
         ptcl_fpos = self._iord_to_fpos[ptcl_ids_this_halo]
