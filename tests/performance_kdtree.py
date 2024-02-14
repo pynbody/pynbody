2e4ac86a134a58693bb1221e259b6f749a40a3e5
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2024-02-14 14:04:27 +0000

    Improve the KDTree performance measurement script

--- tests/performance_kdtree.py
+++ tests/performance_kdtree.py
@@ -1,9 +1,26 @@
+import contextlib
 import sys
 import time
 
 import numpy as np
 
 import pynbody
+
+@contextlib.contextmanager
+def timer(name):
+    start = time.time()
+    yield
+    end = time.time()
+    print(f"{name} took {end-start:.2f}s")
+
+print("""performance_kdtree.py
+
+This script is designed to test the performance of the KDTree.
+It does not test the correctness, for which the normal unit tests should be used.
+
+You can test with different numbers of threads by passing the number of threads as an argument to this script.
+
+""")
 
 try:
     num_threads = int(sys.argv[1])
@@ -24,30 +41,28 @@ f = pynbody.new(dm=Npart)
 f['pos'] = np.random.uniform(size=(Npart,3))
 f['pos'] -= 0.5
 
-start = time.time()
-for cen, rad in zip(centres, radii):
-    print(".",end="")
-    sys.stdout.flush()
-    f[pynbody.filt.Sphere(rad, cen)]
-
-end = time.time()
-
-print(f"spheres without tree time: {end-start:.2f}s")
+with timer("sphere queries without tree"):
+    for cen, rad in zip(centres, radii):
+        print(".",end="")
+        sys.stdout.flush()
+        _ = f[pynbody.filt.Sphere(rad, cen)]
 
 
-start = time.time()
-pynbody.sph.build_tree(f, num_threads=num_threads)
-end = time.time()
-
-print(f"tree build time: {end-start:.2f}s")
-
-start = time.time()
-for cen,rad in zip(centres,radii):
-    print(".", end="")
-    sys.stdout.flush()
-    f[pynbody.filt.Sphere(rad, cen)]
+with timer("tree build"):
+    pynbody.sph.build_tree(f, num_threads=num_threads)
 
 
-end = time.time()
 
-print(f"spheres from tree time: {end-start:.2f}s")
+with timer("sphere queries from tree"):
+    for cen,rad in zip(centres,radii):
+        print(".", end="")
+        sys.stdout.flush()
+        _ = f[pynbody.filt.Sphere(rad, cen)]
+
+
+
+with timer("get smooth"):
+    _ = f['smooth']
+
+with timer("get rho"):
+    _ = f['rho']
