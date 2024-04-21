Update all units to more modern values, and fix swift scalefactor dependencies

tests/array_test.py
@@ -9,6 +9,7 @@ import sys
 import time
 
 import numpy as np
+import numpy.testing as npt
 import pytest
 
 
@@ -115,11 +116,15 @@ def test_unit_array_interaction():
     """Test for issue 113 and related"""
     x = pynbody.units.Unit('1 Mpc')
     y = SA(np.ones(10), 'kpc')
-    assert all(x + y == SA([1.001] * 10, 'Mpc'))
-    assert all(x - y == SA([0.999] * 10, 'Mpc'))
+    npt.assert_allclose(x + y, SA([1.001] * 10, 'Mpc'))
+    npt.assert_allclose(x - y, SA([0.999] * 10, 'Mpc'))
+
     assert (x + y).units == 'Mpc'
-    assert all(y + x == SA([1.001] * 10, 'Mpc'))
-    assert all(y - x == SA([-999.] * 10, 'kpc'))
+
+    npt.assert_allclose(y + x, SA([1.001] * 10, 'Mpc'))
+    npt.assert_allclose(y - x, SA([-999.] * 10, 'kpc'))
+
+
 
 
 def test_dimensionful_comparison():