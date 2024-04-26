c1dffc8c8fbda8d9b7a35ca1f2bb40b5bc7d29b2
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2024-04-26 22:47:26 +0100

    Try another approach to managing warnings in hmf_test.py

--- tests/hmf_test.py
+++ tests/hmf_test.py
@@ -1,26 +1,30 @@
-import pynbody
-import pynbody.analysis.hmf as hmf
-
 import numpy as np
 import pytest
 
-def test_powspec_static():
+import pynbody
+import pynbody.analysis.hmf as hmf
+
+
+def test_powspec_static(recwarn):
     f = pynbody.new()
-    with pytest.warns(RuntimeWarning, match="Assuming default value"):
-        ps = hmf.PowerSpectrumCAMB(f)
+    ps = hmf.PowerSpectrumCAMB(f)
+
+    assert any(["Assuming default value" in str(w.message) for w in recwarn.list])
+    assert any(["assumes Planck 2018" in str(w.message) for w in recwarn.list])
 
     assert np.allclose(ps.k.min(), 1.0e-4)
     assert np.allclose(ps.k.max(), 1.0e4)
     assert np.argmax(ps(ps.k)) == 369
     assert np.allclose(ps(ps.k[369]), 25452.63366178445)
 
-def test_powspec_live():
+def test_powspec_live(recwarn):
     f = pynbody.new()
-    with pytest.warns(RuntimeWarning, match="Assuming default value"):
-        ps = hmf.PowerSpectrumCAMBLive(f)
+
+    ps = hmf.PowerSpectrumCAMBLive(f)
+
+    assert any(["Assuming default value" in str(w.message) for w in recwarn.list])
 
     assert np.allclose(ps.k.min(), 1.0e-4)
     assert np.allclose(ps.k.max(), 1.0e4)
     assert np.argmax(ps(ps.k)) == 368
     assert np.allclose(ps(ps.k[368]), 25985.50574103266)
-
