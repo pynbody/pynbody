Fixed bugs in the plot.stars.schmidlaw function with the binning and the units that are used.

pynbody/plot/stars.py
@@ -813,8 +813,8 @@ def schmidtlaw(sim, center=True, filename=None, pretime='50 Myr',
 
 	# calculate surface densities
 	if radial:
-		ps = profile.Profile(diskstars[youngstars], nbins=bins)
-		pg = profile.Profile(diskgas, nbins=bins)
+		ps = profile.Profile(diskstars[youngstars], nbins=bins, rmin=0, rmax=units.Unit(rmax))
+		pg = profile.Profile(diskgas, nbins=bins, rmin=0, rmax=units.Unit(rmax))
 	else:
 		# make bins 2 kpc
 		nbins = rmax * 2 / binsize
@@ -829,9 +829,9 @@ def schmidtlaw(sim, center=True, filename=None, pretime='50 Myr',
 	if clear:
 		plt.clf()
 
-	plt.loglog(pg['density'].in_units('Msol pc^-2'),
-			   ps['density'].in_units('Msol kpc^-2') / pretime / 1e6, "+",
-			   **kwargs)
+    plt.loglog(pg['density'].in_units('Msol pc^-2'),
+               (ps['density']/pretime).in_units('Msol kpc**-2 yr**-1'), "+",
+               **kwargs)
 
 	if compare:
 		xsigma = np.logspace(np.log10(pg['density'].in_units('Msol pc^-2')).min(),