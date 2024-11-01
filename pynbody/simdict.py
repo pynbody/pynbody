2d599a17a19a4bdf36a3f81589480ffc53002589
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2024-11-01 16:15:02 +0000

    Update documentation for simdict

--- pynbody/simdict.py
+++ pynbody/simdict.py
@@ -1,35 +1,10 @@
 """
-simdict
-=======
-
-This submodule defines an augmented dictionary class
-(:class:`SimDict`) for :class:`~pynbody.snapshot.SimSnap` properties
-where entries need to be managed e.g.  for defining default entries,
-or for ensuring consistency between equivalent properties like
-redshift and scalefactor.
+Defines an augmented dictionary class that manages properties of :class:`~pynbody.snapshot.simsnap.SimSnap` objects
 
 By default, a :class:`SimDict` automatically converts between
 redshift ('z') and scalefactor ('a') and implements default entries
 for cosmological values listed in the [default-cosmology] section of
 the `pynbody` configuration files.
-
-Adding further properties
--------------------------
-
-To add further properties use the SimDict.getter and SimDict.setter decorators.
-For instance, to add a property 'X_copy' which just reflects the value of the
-property 'X', you would use the following code:
-
-.. code-block:: python
-
- @SimDict.getter
- def X_copy(d) :
-     return d['X']
-
- @SimDict.setter
- def X_copy(d, value) :
-     d['X'] = value
-
 """
 
 import warnings
@@ -40,17 +15,57 @@ __all__ = ['SimDict']
 
 
 class SimDict(dict):
+    """A dictionary class for managing properties of a :class:`~pynbody.snapshot.simsnap.SimSnap` object.
+
+    Above the standard dictionary methods, this class also provides automatic derivation of properties,
+    such as converting between redshift and scalefactor, and default values for cosmological parameters.
+
+    To add further properties use the SimDict.getter and SimDict.setter decorators.
+    For instance, to add a property 'X_copy' which just reflects the value of the
+    property 'X', you would use the following code:
+
+    .. code-block:: python
+
+     @SimDict.getter
+     def X_copy(d):
+         return d['X']
+
+     @SimDict.setter
+     def X_copy(d, value):
+         d['X'] = value
+    """
     _getters = {}
     _setters = {}
 
     @staticmethod
     def getter(f):
-        """Define a getter function for all SimDicts"""
+        """Define a getter function for all SimDicts
+
+        See the class documentation for an example of usage.
+
+        Parameters
+        ----------
+
+        f : function
+            The function to be used as a getter. The function should take a single argument, the SimDict object, and
+            return the value of the property. The name of the property is the name of the function.
+
+        """
         SimDict._getters[f.__name__] = f
 
     @staticmethod
     def setter(f):
-        """Define a setter function for all SimDicts"""
+        """Define a setter function for all SimDicts
+
+        See the class documentation for an example of usage.
+
+        Parameters
+        ----------
+
+        f : function
+            The function to be used as a setter. The function should take two arguments, the SimDict object and the
+            value. The name of the property is the name of the function.
+        """
         SimDict._setters[f.__name__] = f
 
     def __getitem__(self, k):
@@ -86,7 +101,7 @@ def z(d, z):
         d["a"] = 1.0 / (1.0 + z)
 
 
-def default_fn(name, value):
+def _default_fn(name, value):
     """Return a getter function for the default name/value pair"""
     def f(d):
         warnings.warn("Assuming default value for property '{}'={:.2e}".format(
@@ -98,4 +113,4 @@ def default_fn(name, value):
     return f
 
 for k in config['default-cosmology']:
-    SimDict.getter(default_fn(k, config['default-cosmology'][k]))
+    SimDict.getter(_default_fn(k, config['default-cosmology'][k]))
