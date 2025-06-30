0c49a49b6330a8fb9e92d47f19ec3f3e3a66ccca
Isaac Alonso Asensio <isaac.alonso.asensio@iac.es>
2025-06-30 16:21:31 +0100

    Expand tests for pkdgrav hdf

--- tests/pkdgravhdf_test.py
+++ tests/pkdgravhdf_test.py
@@ -29,7 +29,6 @@ def multi_snap():
 
 
 def test_npart(multi_snap):
-    print(multi_snap.properties)
     assert len(multi_snap._hdf_files) == multi_snap.properties['NumFilesPerSnapshot']
 
     assert len(multi_snap.g) == multi_snap.properties['NumPart_Total'][0]
@@ -48,22 +47,51 @@ def test_cosmo(snap) :
                        + snap.g['mass'].sum()
                        + snap.bh['mass'].sum()
                        )
+    assert np.allclose(snap.dm['pos'].max(), 0.5, rtol=1e-3)
+    assert np.allclose(snap.dm['pos'].min(), -0.5, rtol=1e-3)
+    max_tform = snap.s['tform'].max()
+    assert snap.properties['time'].in_units('Gyr') > max_tform.in_units('Gyr')
+
+
+def test_units(snap):
+    # From the parameter file:
+    dMsolUnit = 2.747719e13
+    dKpcUnit = 6e3
+    assert np.allclose(snap.dm['pos'].units.in_units('a kpc'), dKpcUnit)
+    assert np.allclose(snap.dm['mass'].units.in_units('Msol'), dMsolUnit)
 
 
 def test_standard_arrays(snap) :
     """Check that the data loading works"""
 
-    for s in [snap] :
-        s.dm['pos']
-        s.gas['pos']
-        s.star['pos']
-        s.bh['pos']
-        s['pos']
-        s['mass']
-        # Load a second time to check that family_arrays still work
-        s.dm['pos']
-        s['vel']
-        s['iord']
-        s.gas['rho']
-        s.star['mass']
-        s.bh['pos']
+    assert len(snap.families()) == 4
+    assert len(snap.dm) == 17576
+    assert len(snap.bh) == 5
+    assert len(snap.gas) == 16834
+    assert len(snap.star) == 737
+
+    snap.dm['pos']
+    snap.gas['pos']
+    snap.star['pos']
+    snap.bh['pos']
+    snap['pos']
+    snap['mass']
+    # Load a second time to check that family_arrays still work
+    snap.dm['pos']
+    snap['vel']
+    snap['iord']
+    snap.gas['rho']
+    snap.star['mass']
+    snap.bh['pos']
+
+    np.testing.assert_allclose(snap.dm['x'][::500],
+                               [-0.44443448, -0.46084865, -0.46529744, -0.45955783, -0.45619511,
+                                -0.14319834, 0.49613796, -0.44908009, -0.00969236, -0.44578614,
+                                0.33156776, 0.45171689, -0.26648184, -0.31669561, -0.0462321 ,
+                                -0.08962971, -0.47501354, -0.48073811, -0.47596492, -0.05170355,
+                                0.00783169, 0.30343563, 0.1798989 , -0.06758307, -0.03830379,
+                                0.07825877, 0.08893577, 0.21393484, 0.21699663, 0.19640893,
+                                0.37019955, 0.44992341, 0.43195645, 0.21873396, 0.26066616,
+                                0.36613503],
+                               rtol=1e-5
+                               )
