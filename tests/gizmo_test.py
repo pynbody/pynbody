f2a680217f2392df81f5c53c67facc8e540cb432
Michael Ryan <mtryan83@gmail.com>
2025-10-23 16:50:03 -0400

    Add test to see if whitespace is allowed

--- tests/gizmo_test.py
+++ tests/gizmo_test.py
@@ -44,3 +44,33 @@ def test_issue_943():
     finally:
         # remove unitfile
         os.remove(unitfile_name)
+
+@pytest.mark.filterwarnings("ignore:No unit information", "ignore:Assuming default value", "ignore:Unable to infer units")
+def test_allow_blank_lines():
+    # Need to create a test snapshot_000.hdf5.units file that 
+    # violates the previous conventions
+    unitfile_name = "testdata/gizmo/snapshot_000.hdf5.units"
+    with open(unitfile_name,"w") as unitfile:
+        unitfile.write("# This is a comment\n") 
+        unitfile.write("\n \t") # empty line with whitespace wasn't allowed
+        unitfile.write("pos: kpc a h^-1\n")
+
+    # Load the gizmo file
+    f = pynbody.load("testdata/gizmo/snapshot_000.hdf5")
+    # this should not raise an error due to whitespace
+    assert f['pos'].units == "Mpc a h^-1"
+    f._override_units_system()
+    assert f['pos'].units == "kpc a h^-1"
+    
+    # Now add a deliberate typo to check it still errors on incorrect formats
+    with open(unitfile_name,"a") as unitfile:
+        unitfile.write("\n")
+        unitfile.write("vel; km s^-1\n") # deliberate typo
+
+    f = pynbody.load("testdata/gizmo/snapshot_000.hdf5")
+    with pytest.raises(OSError):
+        f._override_units_system()
+
+    # remove unitfile
+    os.remove(unitfile_name)
+
