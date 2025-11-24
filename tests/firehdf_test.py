b8ce5fac0baf66331de6ee348153b3ef6ec83988
Jorge Manuel Sarrato Alos <jorgesarrato@gmail.com>
2025-11-24 09:00:09 +0000

    Remove FIRE multisnap read test

--- tests/firehdf_test.py
+++ tests/firehdf_test.py
@@ -6,25 +6,9 @@ import pytest
 import pynbody
 import pynbody.test_utils
 
-# PENDING: find/generate smaller test data, upload to zenodo
-"""
-@pytest.fixture(scope='module', autouse=True)
-def get_data():
-    pynbody.test_utils.ensure_test_data_available("pkdgrav3")
-
-"""
-
 @pytest.fixture
 def snap():
     f = pynbody.load('testdata/FIRE/m11i_res7100/output/snapshot_600.hdf5')
-    yield f
-    del f
-    gc.collect()
-
-
-@pytest.fixture
-def multi_snap():
-    f = pynbody.load('testdata/FIRE/m10q_res30/output/snapdir_600/snapshot_600')
     yield f
     del f
     gc.collect()
