f9be20140649513f0a7a67999145209941c2430e
John Helly <j.c.helly@durham.ac.uk>
2025-12-01 16:31:50 +0000

    Try to test using hdfstream server container

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--hdfstream-server", type=str, default=None, help="hdfstream server URL for the test"
+    )
+
+@pytest.fixture
+def hdfstream_server_url(request):
+    return request.config.getoption("--hdfstream-server")
