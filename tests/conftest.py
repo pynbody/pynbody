0929d024687074953278f8ca4f7284eb9a4cf993
John Helly <j.c.helly@durham.ac.uk>
2026-07-09 11:36:34 +0100

    Add flag to skip certificate verification

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+try:
+    import hdfstream
+except ImportError:
+    hdfstream = None
 import pytest
 
 
@@ -5,7 +9,12 @@ def pytest_addoption(parser):
     parser.addoption(
         "--hdfstream-server", type=str, default=None, help="hdfstream server URL for the test"
     )
+    parser.addoption(
+        "--no-verify-cert", action="store_true", default=False, help="Don't verify SSL certificates if set"
+    )
 
 @pytest.fixture
 def hdfstream_server_url(request):
+    if hdfstream is not None:
+        hdfstream.verify_cert(not request.config.getoption("--no-verify-cert"))
     return request.config.getoption("--hdfstream-server")
