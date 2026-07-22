aee43e24856ea33a6e1fa91a2d7fa44799117a9a
Ben Keller <malzraa@gmail.com>
2026-07-22 00:55:12 -0500

    Use np.ptp to calculate the size of the cube enclosing the particles used to calculate virial radius.

--- pynbody/analysis/halo.py
+++ pynbody/analysis/halo.py
@@ -145,7 +145,7 @@ def virial_radius(sim, cen=None, overden=178, r_max=None, rho_def='matter'):
     """
 
     if r_max is None:
-        r_max = (sim["pos"].max() - sim["pos"].min())
+        r_max = np.ptp(sim['pos'], axis=0).max()
     else:
         if cen is not None:
             sim = sim[filt.Sphere(r_max, cen)]
