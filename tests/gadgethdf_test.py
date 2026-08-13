7563d6e5d8cb7fe60251b77ffa36174d5c36b238
John Helly <j.c.helly@durham.ac.uk>
2026-08-13 12:27:53 +0100

    Repeat GadgetHdfSnap test on EAGLE snapshot

--- tests/gadgethdf_test.py
+++ tests/gadgethdf_test.py
@@ -247,12 +247,13 @@ def test_load_copy_issue_955(snap):
     assert (snap_cop['iord'] == snap[boundary_slice]['iord']).all()
 
 
-def test_remote_open(remote_kwargs):
+@pytest.mark.parametrize('filename',
+                         ["testdata/gadget3/data/snapshot_103/snap_103.hdf5",
+                          "testdata/gadget3/snap_028_z000p000.0.hdf5"])
+def test_remote_open(remote_kwargs, filename):
     """
     Try opening local and remote versions of the same file
     """
-    filename = "testdata/gadget3/data/snapshot_103/snap_103.hdf5"
-
     # Open the local HDF5 file
     local_snap = pynbody.load(filename)
     assert isinstance(local_snap._hdf_files[0], h5py.File)
@@ -263,9 +264,11 @@ def test_remote_open(remote_kwargs):
     assert isinstance(remote_snap._hdf_files[0], hdfstream.RemoteFile)
 
     # Compare file contents
+    assert len(local_snap) == len(remote_snap)
     assert local_snap.families() == remote_snap.families()
     assert local_snap.loadable_keys() == remote_snap.loadable_keys()
     for fam in local_snap.families():
+        assert len(local_snap[fam]) == len(remote_snap[fam])
         assert local_snap[fam].loadable_keys() == remote_snap[fam].loadable_keys()
         assert np.all(local_snap[fam]["pos"] == remote_snap[fam]["pos"])
         assert np.all(local_snap[fam]["iord"] == remote_snap[fam]["iord"])
